91df356f4924f57e435a4be418f6cd44ba134ef1
Marzo Sette Torres Junior <marzojr@gmail.com>
2020-01-02 20:20:49 +0100

    We are not using any of the strings.h AIX functions

--- server/server.cc
+++ server/server.cc
@@ -47,10 +47,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <cstdio>
 #include <cstdlib>
 
-#ifdef _AIX
-#include <strings.h>
-#endif
-
 #if HAVE_NETDB_H
 #include <netdb.h>
 #endif
