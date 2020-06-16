wonst719/exult

commit 72cbedc28d6eeb617d5dac88b39a4c28fb424f07

Applying a few clang-tidy changes on Windows

files/utils.cc
@@ -499,7 +499,7 @@ public:
 	~shell32_wrapper() {
 		FreeLibrary(hLib);
 	}
-	const string Get_local_appdata() {
+	string Get_local_appdata() {
 		/*  Not yet.
 		if (SHGetKnownFolderPath != nullptr)
 		    {
@@ -623,7 +623,7 @@ string Get_home() {
 	else {
 		shell32_wrapper shell32;
 		home_dir = shell32.Get_local_appdata();
-		if (home_dir != "")
+		if (!home_dir.empty())
 			home_dir += "\\Exult";
 		else
 			home_dir = ".";
@@ -717,7 +717,7 @@ void setup_data_dir(
 void setup_program_paths() {
 	string home_dir(Get_home());
 	string config_dir;
-	string savehome_dir(home_dir);
+	string savehome_dir;
 	string gamehome_dir(".");
 
 #ifdef __IPHONEOS__
@@ -727,10 +727,12 @@ void setup_program_paths() {
 #elif defined(MACOSX)
 	config_dir = home_dir;
 	config_dir += "/Library/Preferences";
+	savehome_dir = home_dir;
 	savehome_dir += "/Library/Application Support/Exult";
 	gamehome_dir = "/Library/Application Support/Exult";
 #elif defined(XWIN)
 	config_dir = home_dir;
+	savehome_dir = home_dir;
 	savehome_dir += "/.exult";
 	gamehome_dir = EXULT_DATADIR;
 #endif

gamedat.cc
@@ -753,7 +753,7 @@ bool Game_window::get_saveinfo_zip(const char *fname, char *&name, std::unique_p
 bool Game_window::Restore_level2(
     void *uzf, const char *dirname, int dirlen
 ) {
-	auto unzipfile = static_cast<unzFile>(uzf);
+	auto *unzipfile = static_cast<unzFile>(uzf);
 
 	char oname[50];     // Set up name.
 	char *oname2 = oname + sizeof(GAMEDAT) + dirlen - 1;

mapedit/maps.cc
@@ -226,7 +226,7 @@ void ExultStudio::setup_maps_list(
 	while ((num = Find_next_map(num + 1, 10)) != -1) {
 		char name[40];
 		sprintf(name, "Map #%02x", num);
-		auto ptrnum = reinterpret_cast<gpointer>(uintptr(num));
+		auto *ptrnum = reinterpret_cast<gpointer>(uintptr(num));
 		GtkWidget *item =
 		    Add_menu_item(maps, name, GTK_SIGNAL_FUNC(on_map_activate), ptrnum, group);
 		gtk_object_set_user_data(GTK_OBJECT(item), ptrnum);

shapes/pngio.cc
@@ -136,7 +136,7 @@ int Import_png8(
 	png_read_update_info(png, info);
 	// Allocate pixel buffer.
 	rowbytes = png_get_rowbytes(png, info);
-	auto image = new png_byte[height * rowbytes];
+	auto *image = new png_byte[height * rowbytes];
 	pixels = image;         // Return ->.
 	png_bytep rowptr;       // Read in rows.
 	int r;
@@ -329,7 +329,7 @@ int Import_png32(
 	png_read_update_info(png, info);
 	// Allocate pixel buffer.
 	rowbytes = png_get_rowbytes(png, info);
-	auto image = new png_byte[height * rowbytes];
+	auto *image = new png_byte[height * rowbytes];
 	pixels = image;         // Return ->.
 	png_bytep rowptr = image;   // Read in rows.
 	int stride;         // Distance to next row.

windrag.cc
@@ -336,7 +336,7 @@ Windropsource::GiveFeedback(
 
 // IDataObject implementation
 
-Winstudioobj::Winstudioobj(windragdata pdata)
+Winstudioobj::Winstudioobj(const windragdata& pdata)
 	: data(pdata) {
 	m_cRef = 1;
 	drag_image = nullptr;

windrag.h
@@ -197,7 +197,7 @@ private:
 	windragdata data;
 
 public:
-	Winstudioobj(windragdata pdata);
+	Winstudioobj(const windragdata& pdata);
 	virtual ~Winstudioobj() = default;
 
 	STDMETHOD(QueryInterface)(REFIID iid, void **ppvObject);

commit 32e658a00b14e3e492473c599b06ba23e36e3541

Cleaning up default paths code
Also fixing exult.cfg creation on Windows, which was broken by 72cbedc2.

files/utils.cc
@@ -718,28 +718,65 @@ void setup_data_dir(
 	exit(-1);
 }
 
+static string Get_config_dir(const string& home_dir) {
+#ifdef __IPHONEOS__
+	ignore_unused_variable_warning(home_dir);
+	return ios_get_documents_dir();
+#elif defined(MACOSX)
+	string config_dir(home_dir);
+	config_dir += "/Library/Preferences";
+	return config_dir;
+#else
+	return home_dir;
+#endif
+}
+
+static string Get_savehome_dir(const string& home_dir, const string& config_dir) {
+#ifdef __IPHONEOS__
+	ignore_unused_variable_warning(home_dir);
+	string savehome_dir(config_dir);
+	savehome_dir += "/save";
+	return savehome_dir;
+#elif defined(MACOSX)
+	ignore_unused_variable_warning(config_dir);
+	string savehome_dir(home_dir);
+	savehome_dir += "/Library/Application Support/Exult";
+	return savehome_dir;
+#elif defined(XWIN)
+	ignore_unused_variable_warning(config_dir);
+	string savehome_dir(home_dir);
+	savehome_dir += "/.exult";
+	return savehome_dir;
+#else
+	ignore_unused_variable_warning(config_dir);
+	return home_dir;
+#endif
+}
+
+static string Get_gamehome_dir(const string& home_dir, const string& config_dir) {
+#ifdef __IPHONEOS__
+	ignore_unused_variable_warning(home_dir);
+	string gamehome_dir(config_dir);
+	gamehome_dir += "/game";
+	return gamehome_dir;
+#elif defined(MACOSX)
+	ignore_unused_variable_warning(home_dir, config_dir);
+	return "/Library/Application Support/Exult";
+#elif defined(XWIN)
+	ignore_unused_variable_warning(home_dir, config_dir);
+	return EXULT_DATADIR;
+#else
+	ignore_unused_variable_warning(home_dir, config_dir);
+	return ".";
+#endif
+}
+
 void setup_program_paths() {
 	string home_dir(Get_home());
-	string config_dir;
-	string savehome_dir;
-	string gamehome_dir(".");
+	string config_dir(Get_config_dir(home_dir));
+	string savehome_dir(Get_savehome_dir(home_dir, config_dir));
+	string gamehome_dir(Get_gamehome_dir(home_dir, config_dir));
 
-#ifdef __IPHONEOS__
-	config_dir = ios_get_documents_dir();
-	savehome_dir = config_dir + "/save";
-	gamehome_dir = config_dir + "/game";
-#elif defined(MACOSX)
-	config_dir = home_dir;
-	config_dir += "/Library/Preferences";
-	savehome_dir = home_dir;
-	savehome_dir += "/Library/Application Support/Exult";
-	gamehome_dir = "/Library/Application Support/Exult";
-#elif defined(XWIN)
-	config_dir = home_dir;
-	savehome_dir = home_dir;
-	savehome_dir += "/.exult";
-	gamehome_dir = EXULT_DATADIR;
-#endif
 	if (get_system_path("<HOME>") != ".")
 		add_system_path("<HOME>", home_dir);
 	add_system_path("<CONFIG>", config_dir);