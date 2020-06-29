wonst719/exult

commit 0703bee47303f1df2baac2b9ce4c60422d87007a

Improving memory management in gumps

gumps/VideoOptions_gump.cc
@@ -63,10 +63,26 @@ int VideoOptions_gump::num_game_resolutions = 0;
 
 Image_window::FillMode VideoOptions_gump::startup_fill_mode = static_cast<Image_window::FillMode>(0);
 
+static inline uint32 make_resolution(uint16 width, uint16 height) {
+	return (uint32(width) << 16) | uint32(height);
+}
+
+static inline uint16 get_width(uint32 resolution) {
+	return resolution >> 16;
+}
+
+static inline uint16 get_height(uint32 resolution) {
+	return (resolution & 0xffffu);
+}
+
 static string resolutionstring(int w, int h) {
 	char buf[100];
 	sprintf(buf, "%ix%i", w, h);
 	return buf;
+}
+
+static string resolutionstring(uint32 resolution) {
+	return resolutionstring(get_width(resolution), get_height(resolution));
 }
 
 using VideoOptions_button = CallbackTextButton<VideoOptions_gump>;
@@ -92,69 +108,52 @@ void VideoOptions_gump::rebuild_buttons() {
 
 	// the text arrays are freed by the destructors of the buttons
 
-	std::string *scalers = new std::string[Image_window::NumScalers];
-	for (int i = 0; i < Image_window::NumScalers; i++)
-		scalers[i] = Image_window::get_name_for_scaler(i);
-
+	std::vector<std::string> scalers;
+	for (int i = 0; i < Image_window::NumScalers; i++) {
+		scalers.emplace_back(Image_window::get_name_for_scaler(i));
+	}
 	buttons[id_scaler] = std::make_unique<VideoTextToggle>(this, &VideoOptions_gump::toggle_scaler,
-	        scalers, scaler, Image_window::NumScalers, colx[2], rowy[3], 74);
+	        std::move(scalers), scaler, colx[2], rowy[3], 74);
 
+	std::vector<std::string> game_restext = {"Auto", "320x200"};
 #ifdef __IPHONEOS__
-	//more Game Area modes on iOS devices
-	// because editing the cfg is not as easy
-	// !!!FIXME!!! maybe we could add some for every Exult port?
-	std::string *game_restext = new std::string[5];
-#else
-	std::string *game_restext = new std::string[3];
+	game_restext.emplace_back("400x250");
+	game_restext.emplace_back("480x300");
 #endif
-	game_restext[0] = "Auto";
-	game_restext[1] = "320x200";
-#ifdef __IPHONEOS__
-	game_restext[2] = "400x250";
-	game_restext[3] = "480x300";
-	game_restext[4] = resolutionstring(game_resolutions[2] >> 16, game_resolutions[2] & 0xFFFF);
-#else
-	game_restext[2] = resolutionstring(game_resolutions[2] >> 16, game_resolutions[2] & 0xFFFF);
-#endif
+	game_restext.emplace_back(resolutionstring(game_resolutions[2]));
 
 	buttons[id_game_resolution] = std::make_unique<VideoTextToggle>(this, &VideoOptions_gump::toggle_game_resolution,
-	        game_restext, game_resolution, num_game_resolutions, colx[2], rowy[6], 74);
+	        std::move(game_restext), game_resolution, colx[2], rowy[6], 74);
 
-	std::string *fill_scaler_text = new std::string[2];
-	fill_scaler_text[0] = "Point";
-	fill_scaler_text[1] = "Bilinear";
+	std::vector<std::string> fill_scaler_text = {"Point", "Bilinear"};
 	buttons[id_fill_scaler] = std::make_unique<VideoTextToggle>(this, &VideoOptions_gump::toggle_fill_scaler,
-	        fill_scaler_text, fill_scaler, 2 , colx[2], rowy[7], 74);
+	        std::move(fill_scaler_text), fill_scaler, colx[2], rowy[7], 74);
 
 	int sel_fill_mode;
 	has_ac = false;
 
-	if (fill_mode == Image_window::Fill)
+	if (fill_mode == Image_window::Fill) {
 		sel_fill_mode = 0;
-	else if (fill_mode == Image_window::Fit)
+	} else if (fill_mode == Image_window::Fit) {
 		sel_fill_mode = 1;
-	else if (fill_mode == Image_window::AspectCorrectFit) {
+	} else if (fill_mode == Image_window::AspectCorrectFit) {
 		sel_fill_mode = 1;
 		has_ac = true;
-	} else if (fill_mode == Image_window::Centre)
+	} else if (fill_mode == Image_window::Centre) {
 		sel_fill_mode = 2;
-	else if (fill_mode == Image_window::AspectCorrectCentre) {
+	} else if (fill_mode == Image_window::AspectCorrectCentre) {
 		sel_fill_mode = 2;
 		has_ac = true;
-	} else
+	} else {
 		sel_fill_mode = 3;
-
-	int num_fill_modes = 3;
-	if (startup_fill_mode > Image_window::AspectCorrectCentre) num_fill_modes = 4;
-
-	std::string *fill_mode_text = new std::string[4];
-	fill_mode_text[0] = "Fill";
-	fill_mode_text[1] = "Fit";
-	fill_mode_text[2] = "Centre";
-	fill_mode_text[3] = "Custom";
+	}
+	std::vector<std::string> fill_mode_text = {"Fill", "Fit", "Centre"};
+	if (startup_fill_mode > Image_window::AspectCorrectCentre) {
+		fill_mode_text.emplace_back("Custom");
+	}
 
 	buttons[id_fill_mode] = std::make_unique<VideoTextToggle>(this, &VideoOptions_gump::toggle_fill_mode,
-	        fill_mode_text, sel_fill_mode, num_fill_modes, colx[2], rowy[8], 74);
+	        std::move(fill_mode_text), sel_fill_mode, colx[2], rowy[8], 74);
 
 	rebuild_dynamic_buttons();
 }
@@ -172,25 +171,26 @@ void VideoOptions_gump::rebuild_dynamic_buttons() {
 		num_resolutions = VideoOptions_gump::num_resolutions;
 		resolutions = VideoOptions_gump::resolutions;
 		if (!gwin->get_win()->is_fullscreen())
-			current_res = (gwin->get_win()->get_display_width() << 16) | (gwin->get_win()->get_display_height());
+			current_res = make_resolution(gwin->get_win()->get_display_width(), gwin->get_win()->get_display_height());
 	} else {
 		num_resolutions = VideoOptions_gump::num_win_resolutions;
 		resolutions = VideoOptions_gump::win_resolutions;
-		current_res = (gwin->get_win()->get_display_width() << 16) | (gwin->get_win()->get_display_height());
+		current_res = make_resolution(gwin->get_win()->get_display_width(), gwin->get_win()->get_display_height());
 	}
 
 	int selected_res = 0;
-	std::string *restext = new std::string[num_resolutions + 1];
+	std::vector<std::string> restext;
 	for (int i = 0; i < num_resolutions; i++) {
-		int rw = resolutions[i] >> 16;
-		int rh = resolutions[i] & 0xFFFF;
-		restext[i] = resolutionstring(rw, rh);
-		if (resolutions[i] <= resolution && resolutions[selected_res] < resolutions[i])
+		restext.emplace_back(resolutionstring(resolutions[i]));
+		if (resolutions[i] <= resolution && resolutions[selected_res] < resolutions[i]) {
 			selected_res = i;
-		if (resolutions[i] == current_res) current_res = 0;
+		}
+		if (resolutions[i] == current_res) {
+			current_res = 0;
+		}
 	}
-	if (current_res) {
-		restext[num_resolutions] = resolutionstring(current_res >> 16, current_res & 0xFFFF);
+	if (current_res != 0) {
+		restext.emplace_back(resolutionstring(current_res));
 
 		if (resolutions[num_resolutions] <= resolution && resolutions[selected_res] < resolutions[num_resolutions]) {
 			selected_res = num_resolutions;
@@ -203,7 +203,7 @@ void VideoOptions_gump::rebuild_dynamic_buttons() {
 	resolution = resolutions[selected_res];
 
 	buttons[id_resolution] = std::make_unique<VideoTextToggle>(this, &VideoOptions_gump::toggle_resolution,
-	        restext, selected_res, num_resolutions, colx[2], rowy[1], 74);
+	        std::move(restext), selected_res, colx[2], rowy[1], 74);
 
 	const int max_scales = scaling > 8 && scaling <= 16 ? scaling : 8;
 	const int num_scales = (scaler == Image_window::point ||
@@ -211,14 +211,14 @@ void VideoOptions_gump::rebuild_dynamic_buttons() {
 	                        scaler == Image_window::bilinear) ? max_scales : 1;
 	if (num_scales > 1) {
 		// the text arrays is freed by the destructor of the button
-		std::string *scalingtext = new std::string[num_scales];
+		std::vector<std::string> scalingtext;
 		for (int i = 0; i < num_scales; i++) {
 			char buf[10];
 			snprintf(buf, sizeof(buf), "x%d", i + 1);
-			scalingtext[i] = buf;
+			scalingtext.emplace_back(buf);
 		}
 		buttons[id_scaling] = std::make_unique<VideoTextToggle>(this, &VideoOptions_gump::toggle_scaling,
-		        scalingtext, scaling, num_scales, colx[2], rowy[4], 74);
+		        std::move(scalingtext), scaling, colx[2], rowy[4], 74);
 	} else if (scaler == Image_window::Hq3x || scaler == Image_window::_3xBR)
 		scaling = 2;
 	else if (scaler == Image_window::Hq4x || scaler == Image_window::_4xBR)
@@ -227,11 +227,9 @@ void VideoOptions_gump::rebuild_dynamic_buttons() {
 		scaling = 1;
 
 	if (fill_mode == Image_window::Fit || fill_mode == Image_window::AspectCorrectFit || fill_mode == Image_window::Centre || fill_mode == Image_window::AspectCorrectCentre) {
-		std::string *ac_text = new std::string[2];
-		ac_text[0] = "Disabled";
-		ac_text[1] = "Enabled";
+		std::vector<std::string> ac_text = {"Disabled", "Enabled"};
 		buttons[id_has_ac] = std::make_unique<VideoTextToggle>(this, &VideoOptions_gump::toggle_aspect_correction,
-		        ac_text, has_ac ? 1 : 0, 2, colx[3], rowy[9], 62);
+		        std::move(ac_text), has_ac ? 1 : 0, colx[3], rowy[9], 62);
 	}
 }
 
@@ -239,12 +237,12 @@ void VideoOptions_gump::load_settings(bool Fullscreen) {
 	fullscreen = Fullscreen;
 	int i;
 	setup_video(fullscreen, MENU_INIT);
-	int w = resolution >> 16;
-	int h = resolution & 0xFFFF;
+	int w = get_width(resolution);
+	int h = get_height(resolution);
 	if (resolutions == nullptr) {
 		fullscreen = gwin->get_win()->is_fullscreen() ? 1 : 0;
 		std::map<uint32, Image_window::Resolution> Resolutions = Image_window::Resolutions;
-		if (fullscreen) Resolutions[(w << 16) | h] = Image_window::Resolution();
+		if (fullscreen) Resolutions[make_resolution(w, h)] = Image_window::Resolution();
 
 		num_resolutions = Resolutions.size();
 		resolutions = new uint32[num_resolutions + 1];
@@ -254,15 +252,17 @@ void VideoOptions_gump::load_settings(bool Fullscreen) {
 			resolutions[i++] = it->first;
 
 		// Add in useful window resolutions
-		Resolutions[(320 << 16) | 200] = Image_window::Resolution();
-		Resolutions[(320 << 16) | 240] = Image_window::Resolution();
-		Resolutions[(400 << 16) | 300] = Image_window::Resolution();
-		Resolutions[(512 << 16) | 384] = Image_window::Resolution();
-		Resolutions[(640 << 16) | 400] = Image_window::Resolution();
-		Resolutions[(640 << 16) | 480] = Image_window::Resolution();
-		Resolutions[(960 << 16) | 600] = Image_window::Resolution();
-		Resolutions[(960 << 16) | 720] = Image_window::Resolution();
-		if (!fullscreen)Resolutions[(w << 16) | h] = Image_window::Resolution();
+		Resolutions[make_resolution(320, 200)] = Image_window::Resolution();
+		Resolutions[make_resolution(320, 240)] = Image_window::Resolution();
+		Resolutions[make_resolution(400, 300)] = Image_window::Resolution();
+		Resolutions[make_resolution(512, 384)] = Image_window::Resolution();
+		Resolutions[make_resolution(640, 400)] = Image_window::Resolution();
+		Resolutions[make_resolution(640, 480)] = Image_window::Resolution();
+		Resolutions[make_resolution(960, 600)] = Image_window::Resolution();
+		Resolutions[make_resolution(960, 720)] = Image_window::Resolution();
+		if (!fullscreen) {
+			Resolutions[make_resolution(w, h)] = Image_window::Resolution();
+		}
 		num_win_resolutions = Resolutions.size();
 		win_resolutions = new uint32[num_win_resolutions + 1];
 
@@ -273,8 +273,8 @@ void VideoOptions_gump::load_settings(bool Fullscreen) {
 	if (startup_fill_mode == 0)
 		startup_fill_mode = fill_mode;
 	has_ac = false;
-	int gw = game_resolution >> 16;
-	int gh = game_resolution & 0xFFFF;
+	int gw = get_width(game_resolution);
+	int gh = get_height(game_resolution);
 	if (gw == 0 && gh == 0)
 		game_resolution = 0;
 	else if (gw == 320 && gh == 200)
@@ -284,17 +284,17 @@ void VideoOptions_gump::load_settings(bool Fullscreen) {
 
 	if (num_game_resolutions == 0) {
 		game_resolutions[0] = 0;
-		game_resolutions[1] = (320 << 16) | 200;
+		game_resolutions[1] = make_resolution(320, 200);
 #ifdef  __IPHONEOS__
-		game_resolutions[2] = (400 << 16) | 250;
-		game_resolutions[3] = (480 << 16) | 300;
-		game_resolutions[4] = (gw << 16) | gh;
+		game_resolutions[2] = make_resolution(400, 250);
+		game_resolutions[3] = make_resolution(480, 300);
+		game_resolutions[4] = make_resolution(gw, gh);
 		num_game_resolutions = (game_resolutions[0] != game_resolutions[4]
 							&& game_resolutions[1] != game_resolutions[4]
 							&& game_resolutions[2] != game_resolutions[4]
 							&& game_resolutions[3] != game_resolutions[4]) ? 5 : 4;
 #else
-		game_resolutions[2] = (gw << 16) | gh;
+		game_resolutions[2] = make_resolution(gw, gh);
 		num_game_resolutions = (game_resolutions[0] != game_resolutions[2] && game_resolutions[1] != game_resolutions[2]) ? 3 : 2;
 #endif
 	}
@@ -314,27 +314,22 @@ VideoOptions_gump::VideoOptions_gump() : Modal_gump(nullptr, EXULT_FLX_VIDEOOPTI
 	video_options_gump = this;
 	set_object_area(Rectangle(0, 0, 0, 0), 8, 170);
 
+	const std::vector<std::string> enabledtext = {"Disabled", "Enabled"};
+
 	fullscreen = gwin->get_win()->is_fullscreen();
-	std::string *enabledtext = new std::string[2];
-	enabledtext[0] = "Disabled";
-	enabledtext[1] = "Enabled";
 #ifndef __IPHONEOS__
 	buttons[id_fullscreen] = std::make_unique<VideoTextToggle>(this, &VideoOptions_gump::toggle_fullscreen,
-	        enabledtext, fullscreen, 2, colx[2], rowy[0], 74);
+	        enabledtext, fullscreen, colx[2], rowy[0], 74);
 #endif
 	config->value("config/video/highdpi", highdpi, false);
-	std::string *hdpi_text = new std::string[2];
-	hdpi_text[0] = "Disabled";
-	hdpi_text[1] = "Enabled";
 	buttons[id_high_dpi] = std::make_unique<VideoTextToggle>(this, &VideoOptions_gump::toggle_high_dpi,
-	        hdpi_text, highdpi, 2, colx[2], rowy[2], 74);
+	        enabledtext, highdpi, colx[2], rowy[2], 74);
 	config->value("config/video/share_video_settings", share_settings, false);
-	std::string *yesNO = new std::string[2];
-	yesNO[0] = "No";
-	yesNO[1] = "Yes";
+	
+	std::vector<std::string> yesNO = {"No", "Yes"};
 #ifndef __IPHONEOS__
 	buttons[id_share_settings] = std::make_unique<VideoTextToggle>(this, &VideoOptions_gump::toggle_share_settings,
-	        yesNO, share_settings, 2, colx[5], rowy[11], 40);
+	        std::move(yesNO), share_settings, colx[5], rowy[11], 40);
 #endif
 	o_share_settings = share_settings;
 
@@ -354,10 +349,10 @@ VideoOptions_gump::~VideoOptions_gump() {
 }
 
 void VideoOptions_gump::save_settings() {
-	int resx = resolution >> 16;
-	int resy = resolution & 0xFFFF;
-	int gw = game_resolutions[game_resolution] >> 16;
-	int gh = game_resolutions[game_resolution] & 0xFFFF;
+	int resx = get_width(resolution);
+	int resy = get_height(resolution);
+	int gw = get_width(game_resolutions[game_resolution]);
+	int gh = get_height(game_resolutions[game_resolution]);
 
 	int tgw = gw;
 	int tgh = gh;
@@ -380,10 +375,10 @@ void VideoOptions_gump::save_settings() {
 	gwin->set_all_dirty();
 
 	if (!Countdown_gump::ask("Settings applied.\nKeep?", 20)) {
-		resx = o_resolution >> 16;
-		resy = o_resolution & 0xFFFF;
-		gw = game_resolutions[o_game_resolution] >> 16;
-		gh = game_resolutions[o_game_resolution] & 0xFFFF;
+		resx = get_width(o_resolution);
+		resy = get_height(o_resolution);
+		gw = get_width(game_resolutions[o_game_resolution]);
+		gh = get_height(game_resolutions[o_game_resolution]);
 		bool o_fullscreen;
 		config->value("config/video/fullscreen", o_fullscreen, true);
 		if (fullscreen != o_fullscreen) // use old settings from the config

gumps/AudioOptions_gump.cc
@@ -100,21 +100,17 @@ void AudioOptions_gump::rebuild_buttons() {
 
 	if (!audio_enabled) return;
 
-	std::string *sampleRates = new std::string[5];
-	sampleRates[0] = "11025";
-	sampleRates[1] = "22050";
-	sampleRates[2] = "44100";
-	sampleRates[3] = "48000";
-	sampleRates[4] = sample_rate_str;
+	std::vector<std::string> sampleRates = {"11025", "22050", "44100", "48000"};
+	if (std::find(sampleRates.cbegin(), sampleRates.cend(), sample_rate_str) != sampleRates.cend()) {
+		sampleRates.push_back(sample_rate_str);
+	}
 
 	buttons[id_sample_rate] = std::make_unique<AudioTextToggle>(this, &AudioOptions_gump::toggle_sample_rate,
-	        sampleRates, sample_rate, num_sample_rates, colx[2], rowy[1], 59);
+	        std::move(sampleRates), sample_rate, colx[2], rowy[1], 59);
 
-	std::string *speaker_types = new std::string[2];
-	speaker_types[0] = "Mono";
-	speaker_types[1] = "Stereo";
+	std::vector<std::string> speaker_types = {"Mono", "Stereo"};
 	buttons[id_speaker_type] = std::make_unique<AudioTextToggle>(this, &AudioOptions_gump::toggle_speaker_type,
-	        speaker_types, speaker_type, 2, colx[2], rowy[2], 59);
+	        std::move(speaker_types), speaker_type, colx[2], rowy[2], 59);
 
 	// music on/off
 	buttons[id_music_enabled] = std::make_unique<AudioEnabledToggle>(this, &AudioOptions_gump::toggle_music_enabled,
@@ -123,22 +119,17 @@ void AudioOptions_gump::rebuild_buttons() {
 		rebuild_midi_buttons();
 
 	// sfx on/off
-	std::string *sfx_options = new std::string[nsfxopts];
-	sfx_options[0] = "Disabled";
-	int i = 1;
-	if (have_digital_sfx())
-#ifndef ENABLE_MIDISFX
-		sfx_options[i++] = "Enabled";
-#else
-		sfx_options[i++] = "Digital";
+	std::vector<std::string> sfx_options = {"Disabled", "Digital"};
+#ifdef ENABLE_MIDISFX
 	midi_state = -1;
 	if (have_midi_pack) {
-		sfx_options[i++] = "Midi";
+		sfx_options.emplace_back("Midi");
 		midi_state = i - 1;
 	}
 #endif
+
 	buttons[id_sfx_enabled] = std::make_unique<AudioTextToggle>(this, &AudioOptions_gump::toggle_sfx_enabled,
-	        sfx_options, sfx_enabled, nsfxopts, colx[2], rowy[10], 59);
+	        std::move(sfx_options), sfx_enabled, colx[2], rowy[10], 59);
 	if (sfx_enabled)
 		rebuild_sfx_buttons();
 
@@ -159,15 +150,14 @@ void AudioOptions_gump::rebuild_midi_buttons() {
 	        midi_ogg_enabled, colx[2], rowy[5], 59);
 
 	unsigned int num_midi_drivers = MidiDriver::getDriverCount();
-	std::string *midi_drivertext = new std::string[num_midi_drivers + 1];
-	unsigned int i;
-	for (i = 0; i < num_midi_drivers; i++)
-		midi_drivertext[i] = MidiDriver::getDriverName(i);
-	midi_drivertext[i] = "Default";
+	std::vector<std::string> midi_drivertext;
+	for (unsigned int i = 0; i < num_midi_drivers; i++)
+		midi_drivertext.emplace_back(MidiDriver::getDriverName(i));
+	midi_drivertext.emplace_back("Default");
 
 	// midi driver
 	buttons[id_midi_driver] = std::make_unique<AudioTextToggle>(this, &AudioOptions_gump::toggle_midi_driver,
-	        midi_drivertext, midi_driver, num_midi_drivers + 1, colx[2] - 15, rowy[6], 74);
+	        std::move(midi_drivertext), midi_driver, colx[2] - 15, rowy[6], 74);
 
 	// looping on/off
 	buttons[id_music_looping] = std::make_unique<AudioEnabledToggle>(this, &AudioOptions_gump::toggle_music_looping,
@@ -184,26 +174,23 @@ void AudioOptions_gump::rebuild_sfx_buttons() {
 	if (!sfx_enabled)
 		return;
 	else if (sfx_enabled == 1 && have_digital_sfx() && !gwin->is_in_exult_menu()) {
-		std::string *sfx_digitalpacks = new std::string[nsfxpacks];
-		int i = 0;
+		std::vector<std::string> sfx_digitalpacks;
 		if (have_roland_pack)
-			sfx_digitalpacks[i++] = "Roland MT-32";
+			sfx_digitalpacks.emplace_back("Roland MT-32");
 		if (have_blaster_pack)
-			sfx_digitalpacks[i++] = "Sound Blaster";
+			sfx_digitalpacks.emplace_back("Sound Blaster");
 		if (have_custom_pack)
-			sfx_digitalpacks[i++] = "Custom";
+			sfx_digitalpacks.emplace_back("Custom");
 		buttons[id_sfx_pack] = std::make_unique<AudioTextToggle>(this, &AudioOptions_gump::toggle_sfx_pack,
-		        sfx_digitalpacks, sfx_package, nsfxpacks, colx[2] - 33, rowy[11], 92);
+		        std::move(sfx_digitalpacks), sfx_package, colx[2] - 33, rowy[11], 92);
 	}
 #ifdef ENABLE_MIDISFX
 	else if (sfx_enabled == midi_state) {
-		std::string *sfx_conversiontext = new std::string[2];
-		sfx_conversiontext[0] = "None";
-		sfx_conversiontext[1] = "GS";
+		std::vector<std::string> sfx_conversiontext = {"None", "GS"};
 
 		// sfx conversion
 		buttons[id_sfx_pack] = std::make_unique<AudioTextToggle>(this, &AudioOptions_gump::toggle_sfx_pack,
-		        sfx_conversiontext, sfx_conversion == 5 ? 1 : 0, 2, colx[2], rowy[11], 59);
+		        std::move(sfx_conversiontext), sfx_conversion == 5 ? 1 : 0, colx[2], rowy[11], 59);
 	}
 #endif
 }
@@ -221,31 +208,24 @@ void AudioOptions_gump::rebuild_mididriveroption_buttons() {
 			string_size = 4;
 		}
 #endif
-		std::string *midi_conversiontext = new std::string[string_size];
-		midi_conversiontext[0] = std::string("Fake MT32");
-		midi_conversiontext[1] = std::string("GM");
-		midi_conversiontext[2] = std::string("GS");
-		midi_conversiontext[3] = std::string("GS127");
-		if (string_size == 5)
-			midi_conversiontext[4] = std::string("MT32");
+		std::vector<std::string> midi_conversiontext = {"Fake MT32", "GM", "GS", "GS127"};
+		if (string_size == 5) {
+			midi_conversiontext.emplace_back("MT32");
+		}
 
 		// midi conversion
 		buttons[id_midi_conv] = std::make_unique<AudioTextToggle>(this, &AudioOptions_gump::toggle_midi_conv,
-		        midi_conversiontext, midi_conversion, string_size, colx[2] - 7, rowy[7], 66);
+		        std::move(midi_conversiontext), midi_conversion, colx[2] - 7, rowy[7], 66);
 	} else {
 		buttons[id_midi_conv].reset();
 	}
 
 	if (s != "FMOpl" && s != "Disabled") {
-		std::string *midi_reverbchorustext = new std::string[4];
-		midi_reverbchorustext[0] = std::string("Disabled");
-		midi_reverbchorustext[1] = std::string("Reverb");
-		midi_reverbchorustext[2] = std::string("Chorus");
-		midi_reverbchorustext[3] = std::string("Both");
+		std::vector<std::string> midi_reverbchorustext = {"Disabled", "Reverb", "Chorus", "Both"};
 
 		// reverb/chorus combo
 		buttons[id_midi_effects] = std::make_unique<AudioTextToggle>(this, &AudioOptions_gump::toggle_midi_effects,
-		        midi_reverbchorustext, midi_reverb_chorus, 4, colx[2], rowy[8], 59);
+		        std::move(midi_reverbchorustext), midi_reverb_chorus, colx[2], rowy[8], 59);
 	} else {
 		buttons[id_midi_effects].reset();
 	}

gumps/GameplayOptions_gump.cc
@@ -64,7 +64,6 @@ static string framestring(int fr) {
 
 #ifndef __IPHONEOS__
 static const char *pathfind_texts[3] = {"no", "single", "double"};
-static int num_pathfind_texts = 3;
 #endif
 
 using GameplayOptions_button = CallbackTextButton<GameplayOptions_gump>;
@@ -81,48 +80,19 @@ void GameplayOptions_gump::cancel() {
 }
 
 void GameplayOptions_gump::build_buttons() {
-	std::string *stats = new std::string[4];
-	stats[0] = "Disabled";
-	stats[1] = "Left";
-	stats[2] = "Middle";
-	stats[3] = "Right";
-
-	std::string *textbgcolor = new std::string[18];
-	textbgcolor[0] = "Disabled";
-	textbgcolor[1] = "Solid Light Gray";
-	textbgcolor[2] = "Dark Purple";
-	textbgcolor[3] = "Bright Yellow";
-	textbgcolor[4] = "Light Blue";
-	textbgcolor[5] = "Dark Green";
-	textbgcolor[6] = "Dark Red";
-	textbgcolor[7] = "Purple";
-	textbgcolor[8] = "Orange";
-	textbgcolor[9] = "Light Gray";
-	textbgcolor[10] = "Green";
-	textbgcolor[11] = "Yellow";
-	textbgcolor[12] = "Pale Blue";
-	textbgcolor[13] = "Dark Green";
-	textbgcolor[14] = "Red";
-	textbgcolor[15] = "Bright White";
-	textbgcolor[16] = "Dark gray";
-	textbgcolor[17] = "White";
-
-	std::string *smooth_text = new std::string[5];
-	smooth_text[0] = "Disabled";
-	smooth_text[1] = "25%";
-	smooth_text[2] = "50%";
-	smooth_text[3] = "75%";
-	smooth_text[4] = "100%";
-
-	std::string *pathfind_text = new std::string[3];
-	pathfind_text[0] = "Disabled";
-	pathfind_text[1] = "Single";
-	pathfind_text[2] = "Double";
-
+	std::vector<std::string> stats = {"Disabled", "Left", "Middle", "Right"};
 	buttons[id_facestats] = std::make_unique<GameplayTextToggle>(this, &GameplayOptions_gump::toggle_facestats,
-	        stats, facestats, 4, colx[3], rowy[0], 59);
+	        std::move(stats), facestats, colx[3], rowy[0], 59);
+
+	std::vector<std::string> textbgcolor = {
+		"Disabled",
+		"Solid Light Gray", "Dark Purple", "Bright Yellow", "Light Blue",
+		"Dark Green", "Dark Red", "Purple", "Orange", "Light Gray", "Green",
+		"Yellow", "Pale Blue", "Dark Green", "Red", "Bright White",
+		"Dark gray", "White"
+	};
 	buttons[id_text_bg] = std::make_unique<GameplayTextToggle>(this, &GameplayOptions_gump::toggle_text_bg,
-	        textbgcolor, text_bg, 18, colx[3] - 41, rowy[1], 100);
+	        std::move(textbgcolor), text_bg, colx[3] - 41, rowy[1], 100);
 	if (sman->can_use_paperdolls() && (GAME_BG ||
 	                                   Game::get_game_type() == EXULT_DEVEL_GAME))
 		buttons[id_paperdolls] = std::make_unique<GameplayEnabledToggle>(this, &GameplayOptions_gump::toggle_paperdolls,
@@ -138,17 +108,21 @@ void GameplayOptions_gump::build_buttons() {
 #ifndef __IPHONEOS__
 	buttons[id_rightclick_close] = std::make_unique<GameplayEnabledToggle>(this, &GameplayOptions_gump::toggle_rightclick_close,
 	        rightclick_close, colx[3], rowy[6], 59);
+
+	std::vector<std::string> pathfind_text = {"Disabled", "Single", "Double"};
 	buttons[id_right_pathfind] = std::make_unique<GameplayTextToggle>(this, &GameplayOptions_gump::toggle_right_pathfind,
-	        pathfind_text, right_pathfind, num_pathfind_texts, colx[3], rowy[7], 59);
+	        std::move(pathfind_text), right_pathfind, colx[3], rowy[7], 59);
 #endif
 	buttons[id_gumps_pause] = std::make_unique<GameplayEnabledToggle>(this, &GameplayOptions_gump::toggle_gumps_pause,
 	        gumps_pause, colx[3], rowy[8], 59);
 	buttons[id_cheats] = std::make_unique<GameplayEnabledToggle>(this, &GameplayOptions_gump::toggle_cheats,
 	        cheats, colx[3], rowy[9], 59);
 	buttons[id_frames] = std::make_unique<GameplayTextToggle>(this, &GameplayOptions_gump::toggle_frames,
-	        frametext, frames, num_framerates, colx[3], rowy[10], 59);
+	        frametext, frames, colx[3], rowy[10], 59);
+
+	std::vector<std::string> smooth_text = {"Disabled", "25%", "50%", "75%", "100%"};
 	buttons[id_smooth_scrolling] = std::make_unique<GameplayTextToggle>(this, &GameplayOptions_gump::toggle_smooth_scrolling,
-	        smooth_text, smooth_scrolling, 5, colx[3], rowy[2], 59);
+	        std::move(smooth_text), smooth_scrolling, colx[3], rowy[2], 59);
 }
 
 void GameplayOptions_gump::load_settings() {
@@ -170,25 +144,24 @@ void GameplayOptions_gump::load_settings() {
 	text_bg = gwin->get_text_bg() + 1;
 	gumps_pause = !gumpman->gumps_dont_pause_game();
 	int realframes = 1000 / gwin->get_std_delay();
-	int i;
 
 	frames = -1;
 	framerates[num_default_rates] = realframes;
-	for (i = 0; i < num_default_rates; i++) {
+	for (int i = 0; i < num_default_rates; i++) {
 		if (realframes == framerates[i]) {
 			frames = i;
 			break;
 		}
 	}
 
-	num_framerates = num_default_rates;
+	int num_framerates = num_default_rates;
 	if (frames == -1) {
 		num_framerates++;
 		frames = num_default_rates;
 	}
-	frametext = new string[num_framerates];
-	for (i = 0; i < num_framerates; i++) {
-		frametext[i] = framestring(framerates[i]);
+	frametext.clear();
+	for (int i = 0; i < num_framerates; i++) {
+		frametext.emplace_back(framestring(framerates[i]));
 	}
 	smooth_scrolling = gwin->is_lerping_enabled() / 25;
 }

gumps/GameplayOptions_gump.h
@@ -43,8 +43,7 @@ private:
 	int right_pathfind;
 	int gumps_pause;
 
-	std::string *frametext;
-	int num_framerates;
+	std::vector<std::string> frametext;
 	int smooth_scrolling;
 
 	enum button_ids {

gumps/Gump_ToggleButton.cc
@@ -63,6 +63,7 @@ void Gump_ToggleButton::unpush(
 
 bool Gump_ToggleTextButton::activate(int button) {
 	int delta;
+	int numselections = selections.size();
 	if (button == 1)
 		delta = 1;
 	else if (button == 3)

commit 5518c27ffb76a27e1832b2aaca362f21382a3fed

Fixing issue #47
"Video options gump slightly messed up by clang-tidy and previously in
merged-devel"

Part was broken in 0703bee4, part in fc65495.

Made game_restext fully dynamic, based on game_resolutions. This makes
it not depend on OS anymore.

gumps/VideoOptions_gump.cc
@@ -108,19 +108,28 @@ void VideoOptions_gump::rebuild_buttons() {
 
 	// the text arrays are freed by the destructors of the buttons
 
-	std::vector<std::string> scalers(Image_window::NumScalers);
+	std::vector<std::string> scalers;
+	scalers.reserve(Image_window::NumScalers);
 	for (int i = 0; i < Image_window::NumScalers; i++) {
 		scalers.emplace_back(Image_window::get_name_for_scaler(i));
 	}
 	buttons[id_scaler] = std::make_unique<VideoTextToggle>(this, &VideoOptions_gump::toggle_scaler,
 	        std::move(scalers), scaler, colx[2], rowy[3], 74);
 
-	std::vector<std::string> game_restext = {"Auto", "320x200"};
-#ifdef __IPHONEOS__
-	game_restext.emplace_back("400x250");
-	game_restext.emplace_back("480x300");
-#endif
-	game_restext.emplace_back(resolutionstring(game_resolutions[2]));
+	std::vector<std::string> game_restext;
+	game_restext.reserve(num_game_resolutions);
+	for (auto *it = std::begin(game_resolutions); it != std::begin(game_resolutions) + num_game_resolutions;  ++it) {
+		uint32 res = *it;
+		std::string restext;
+		if (res == 0) {
+			restext = "Auto";
+		} else {
+			restext = std::to_string(get_width(res));
+			restext += 'x';
+			restext += std::to_string(get_height(res));
+		}
+		game_restext.emplace_back(std::move(restext));
+	}
 
 	buttons[id_game_resolution] = std::make_unique<VideoTextToggle>(this, &VideoOptions_gump::toggle_game_resolution,
 	        std::move(game_restext), game_resolution, colx[2], rowy[6], 74);