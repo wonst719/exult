fa6f4f717826a0a4821e49d7fa75a16fce4f0b5f
Marzo Sette Torres Junior <marzojr@gmail.com>
2021-02-02 23:55:15 +0100

    Fixing Travis build on OSX

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -15,5 +15,5 @@ sudo port -q selfupdate | cat
 sudo port -q upgrade outdated | cat
 
 # Install actual dependencies
-sudo port -q install automake libtool pkgconfig libvorbis libpng zlib | cat
+sudo port -q install automake libtool pkgconfig libvorbis munt-mt32emu libpng zlib | cat
 sudo port -q install libsdl2 gtk3 +x11 | cat
