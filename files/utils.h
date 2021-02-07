wonst719/exult

commit e0275566387c45028fd48b25dc0ad68bea13118d

Adding function to obtain size of file
Without triggering undefined behavior. And making use of it in databuf.h. And some more const-correctness in databuf.h.

files/utils.h
@@ -389,6 +389,18 @@ inline void WriteNhigh(
 		Write1(out, static_cast<uint8>(val >>(8 * i)));
 }
 
+/*
+ *  Get file size without undefined behavior.
+ */
+inline size_t get_file_size(std::istream& in) {
+	const auto start = in.tellg();
+	in.seekg(0);
+	in.ignore(std::numeric_limits<std::streamsize>::max());
+	size_t len = in.gcount();
+	in.seekg(start);
+	return len;
+}
+
 bool U7open(
     std::ifstream &in,          // Input stream to open.
     const char *fname,          // May be converted to upper-case.

files/databuf.h
@@ -61,10 +61,10 @@ public:
 
 	virtual void seek(size_t) = 0;
 	virtual void skip(std::streamoff) = 0;
-	virtual size_t getSize() = 0;
-	virtual size_t getPos() = 0;
-	virtual bool eof() = 0;
-	virtual bool good() {
+	virtual size_t getSize() const = 0;
+	virtual size_t getPos() const = 0;
+	virtual bool eof() const = 0;
+	virtual bool good() const {
 		return true;
 	}
 	virtual void clear_error() { }
@@ -140,19 +140,15 @@ public:
 		in->seekg(pos, std::ios::cur);
 	}
 
-	size_t getSize() final {
-		size_t pos = in->tellg();
-		in->seekg(0, std::ios::end);
-		size_t len = in->tellg();
-		in->seekg(pos);
-		return len;
+	size_t getSize() const final {
+		return get_file_size(*in);
 	}
 
-	size_t getPos() final {
+	size_t getPos() const final {
 		return in->tellg();
 	}
 
-	bool eof() final {
+	bool eof() const final {
 		in->get();
 		bool ret = in->eof();
 		if (!ret) {
@@ -160,7 +156,7 @@ public:
 		}
 		return ret;
 	}
-	bool good() final {
+	bool good() const final {
 		return in->good();
 	}
 	void clear_error() final {
@@ -254,11 +250,11 @@ public:
 		buf_ptr += pos;
 	}
 
-	size_t getSize() final {
+	size_t getSize() const final {
 		return size;
 	}
 
-	size_t getPos() final {
+	size_t getPos() const final {
 		return buf_ptr - buf;
 	}
 
@@ -266,11 +262,11 @@ public:
 		return buf_ptr;
 	}
 
-	bool eof() final {
+	bool eof() const final {
 		return buf_ptr >= buf + size;
 	}
 
-	bool good() final {
+	bool good() const final {
 		return (buf != nullptr) && (size != 0U);
 	}
 
@@ -344,10 +340,10 @@ public:
 
 	virtual void seek(size_t) = 0;
 	virtual void skip(std::streamoff) = 0;
-	virtual size_t getSize() = 0;
-	virtual size_t getPos() = 0;
+	virtual size_t getSize() const = 0;
+	virtual size_t getPos() const = 0;
 	virtual void flush() { }
-	virtual bool good() {
+	virtual bool good() const {
 		return true;
 	}
 	virtual void clear_error() { }
@@ -400,22 +396,18 @@ public:
 		out->seekp(pos, std::ios::cur);
 	}
 
-	size_t getSize() final {
-		size_t pos = out->tellp();
-		out->seekp(0, std::ios::end);
-		size_t len = out->tellp();
-		out->seekp(pos);
-		return len;
+	size_t getSize() const final {
+		return std::numeric_limits<size_t>::max();
 	}
 
-	size_t getPos() final {
+	size_t getPos() const final {
 		return out->tellp();
 	}
 
 	void flush() final {
 		out->flush();
 	}
-	bool good() final {
+	bool good() const final {
 		return out->good();
 	}
 	void clear_error() final {
@@ -496,11 +488,11 @@ public:
 		buf_ptr += pos;
 	}
 
-	size_t getSize() final {
+	size_t getSize() const final {
 		return size;
 	}
 
-	size_t getPos() final {
+	size_t getPos() const final {
 		return buf_ptr - buf;
 	}
 

commit 2fa3ded3347ceb741555f4552c015e4612e78308

Fixing segfault on fonts caused by e0275566

files/utils.h
@@ -394,7 +394,6 @@ inline void WriteNhigh(
  */
 inline size_t get_file_size(std::istream& in) {
 	const auto start = in.tellg();
-	in.seekg(0);
 	in.ignore(std::numeric_limits<std::streamsize>::max());
 	size_t len = in.gcount();
 	in.seekg(start);

shapes/vgafile.cc
@@ -984,7 +984,7 @@ void Shape::load(
 	reset();
 	auto frame = make_unique<Shape_frame>();
 	size_t location = shape_source->getPos();
-	uint32 shapelen = shape_source->getSize();
+	uint32 shapelen = shape_source->getSize() - location;
 	// Read frame 0 & get frame count.
 	create_frames_list(frame->read(shape_source, location, shapelen, 0));
 	store_frame(std::move(frame), 0);