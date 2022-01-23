c82d9a95fed066124beef846dd607a5e3196ad2d
DominusExult <domiman+github@gmail.com>
2022-01-23 12:21:40 +0100

    Fix displayed names of Alsa and UnixSeqDevice in the Audio Options
    Alsa needed to be capitalized and UnixSeqDriver was renamed to UnixSeq to prevent an overflow of the button.

--- audio/midi_drivers/UnixSeqMidiDriver.cpp
+++ audio/midi_drivers/UnixSeqMidiDriver.cpp
@@ -31,7 +31,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <cerrno>
 
 const MidiDriver::MidiDriverDesc UnixSeqMidiDriver::desc =
-		MidiDriver::MidiDriverDesc ("UnixSeqDevice", createInstance);
+		MidiDriver::MidiDriverDesc ("UnixSeq", createInstance);
 
 #define SEQ_MIDIPUTC 5
 #define SEQ_DEVICE "/dev/sequencer"
