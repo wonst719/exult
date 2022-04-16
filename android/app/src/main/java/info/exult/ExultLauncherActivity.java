4c77cab0476364ccadecb7a9d50b9f8ae6bb5ad6
Ken Cecka <ceckak@uw.edu>
2022-04-15 22:28:10 -0700

    Add autolaunch checkbox
    This adds a checkbox to the launcher which is off by default, but can be turned on so that starting the app will bypass the launcher UI and go directly into Exult.  Users can always exit the Exult game engine to get back to the launcher UI if they want to make changes or disable autolaunch.

--- android/app/src/main/java/info/exult/ExultLauncherActivity.java
+++ android/app/src/main/java/info/exult/ExultLauncherActivity.java
@@ -18,6 +18,9 @@
 
 package info.exult;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,10 +29,46 @@ import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 public class ExultLauncherActivity extends AppCompatActivity {
+  private SharedPreferences m_sharedPreferences;
+
+  // This feels pretty hacky, but haven't figured out a better way to detect when the Activity
+  // is returning from a launch versus being opened and eligble to auto-launch.
+  private boolean m_launched = false;
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    if (getAutoLaunch()) {
+      if (!m_launched) {
+        launchExult();
+      } else {
+        m_launched = false;
+      }
+    }
+  }
+
+  private static final String AUTO_LAUNCH_KEY = "autoLaunch";
+
+  public void setAutoLaunch(boolean autoLaunch) {
+    m_sharedPreferences.edit().putBoolean(AUTO_LAUNCH_KEY, autoLaunch).commit();
+  }
+
+  public boolean getAutoLaunch() {
+    return m_sharedPreferences.getBoolean(AUTO_LAUNCH_KEY, false);
+  }
+
+  public void launchExult() {
+    m_launched = true;
+    Intent launchExultIntent = new Intent(this, ExultActivity.class);
+    startActivity(launchExultIntent);
+  }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    m_sharedPreferences = getSharedPreferences(getClass().getSimpleName(), Context.MODE_PRIVATE);
 
     ViewPager2 viewPager = findViewById(R.id.view_pager);
     TabLayout tabLayout = findViewById(R.id.tabs);
--- android/app/src/main/java/info/exult/LauncherFragment.java
+++ android/app/src/main/java/info/exult/LauncherFragment.java
@@ -18,20 +18,33 @@
 
 package info.exult;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import androidx.fragment.app.Fragment;
 
 public class LauncherFragment extends Fragment implements View.OnClickListener {
+  // Probably not ideal to be assuming/referencing the parent activity from a fragment,
+  // but I haven't come up with a better way to handle auto-launch yet.
+  private ExultLauncherActivity getExultLauncherActivity() {
+    return (ExultLauncherActivity) getActivity();
+  }
+
   @Override
   public View onCreateView(
       LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
     View view = inflater.inflate(R.layout.launcher_card, container, false);
+
+    // Route auto-launch toggles to this class
+    CheckBox autoLaunchCheckBox = (CheckBox) view.findViewById(R.id.autoLaunchCheckBox);
+    autoLaunchCheckBox.setOnClickListener(this);
+
+    // Set the initial auto-launch state
+    autoLaunchCheckBox.setChecked(getExultLauncherActivity().getAutoLaunch());
 
     // Route launch clicks to this class
     Button launchExultButton = (Button) view.findViewById(R.id.launchExultButton);
@@ -43,16 +56,16 @@ public class LauncherFragment extends Fragment implements View.OnClickListener {
   @Override
   public void onClick(View view) {
     switch (view.getId()) {
+      case R.id.autoLaunchCheckBox:
+        CheckBox checkBox = (CheckBox) view;
+        boolean checked = checkBox.isChecked();
+        getExultLauncherActivity().setAutoLaunch(checked);
+        break;
       case R.id.launchExultButton:
-        launchExult();
+        getExultLauncherActivity().launchExult();
         break;
       default:
         break;
     }
-  }
-
-  private void launchExult() {
-    Intent launchExultIntent = new Intent(getActivity(), ExultActivity.class);
-    startActivity(launchExultIntent);
   }
 }
