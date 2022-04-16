6c881ad0f79842f02e0ea6c6b6974c387ef7c551
Ken Cecka <ceckak@uw.edu>
2022-04-15 22:28:10 -0700

    Add Games Tab
    This patch adds a tab for installing Ultima game data files.

--- android/app/src/main/java/info/exult/ViewPagerAdapter.java
+++ android/app/src/main/java/info/exult/ViewPagerAdapter.java
@@ -37,6 +37,7 @@ public class ViewPagerAdapter extends FragmentStateAdapter {
 
   private static final TabData[] TABS = {
     new TabData("Launcher", LauncherFragment.class),
+    new TabData("Games", GamesFragment.class),
     new TabData("Console Log", ConsoleLogFragment.class)
   };
 
