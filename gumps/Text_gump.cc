68bfbc74abaf4a046bcae07df2cec28a1060c8c4
wonst719 <wonst719@gmail.com>
2022-07-17 20:15:27 +0900

    Support conversion from Korean to serpentine

--- gumps/Text_gump.cc
+++ gumps/Text_gump.cc
@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "gamewin.h"
 #include "Text_gump.h"
+#include "korean/ucs2kstable.h"
 
 using std::strchr;
 using std::strcpy;
@@ -37,6 +38,32 @@ void Text_gump::add_text(
     const char *str
 ) {
 	int slen = strlen(str);     // Length of new text.
+
+	std::string serpentine = "";
+	if (font == 8) {
+		for (int i = 0; i < slen; i++) {
+			if (str[i] & 0x80) {
+				unsigned char hi = str[i];
+				unsigned char lo = str[i + 1];
+				unsigned short uni = KSToUnicode((hi << 8) | lo);
+				int first, vowel, final;
+				DecomposeUnicode(uni, first, vowel, final);
+				if (first >= 0)
+					serpentine += 'c' + first;
+				if (vowel >= 0)
+					serpentine += 'a' + vowel;
+				//if (final >= 0)
+				//	serpentine += 'a' + final;
+
+				i++;
+			} else {
+				serpentine += str[i];
+			}
+		}
+		str = serpentine.c_str();
+		slen = strlen(str);
+	}
+
 	// Allocate new space.
 	char *newtext = new char[textlen + (textlen != 0) + slen + 1];
 	if (textlen) {          // Copy over old.
