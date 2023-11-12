765b368f72ead15a81a993c3d270dcc9aed61484
Alexis Ryan <the.wench@wenchy.net>
2023-11-13 01:05:49 +1100

    Added some extra platform information to version.cc mostly Windows stuff but also now outputs the build architechture

--- version.cc
+++ version.cc
@@ -42,6 +42,39 @@ void getVersionInfo(std::ostream &out) {
 	out << "Exult version " << VERSION << std::endl;
 
 	/*
+	* 2. Build Architechture
+	*/
+	out << "Build Architechture: ";
+	
+	// AMD64 x86_64
+#if defined(__amd64__) || defined(__amd64) || defined(__amd64__) \
+|| defined(__amd64) || defined(_M_X64) || defined(_M_AMD64)
+	out << "x86_64";
+	// ARM THUMB
+#elif defined(__thumb__) || defined(__TARGET_ARCH_THUMB) || defined(_M_ARMT) 
+		out << "ARM Thumb";
+#elif defined(__arm__) || defined(__TARGET_ARCH_ARM) || defined(_ARM) 
+|| defined(_M_ARM) || defined(__arm)
+	out << "ARM";
+	// ARM64
+#elif defined(__aarch64__) || defined(_M_ARM64) 
+			cout << "ARM64";
+
+			//X86 
+#elif defined(i386) || defined(__i386) || defined(__i386__) \
+|| defined(__i386) || defined(_M_IX86) || defined(__386)
+	out << "x86";
+#elif defined(__riscv)
+	out << "RISC-V"
+#else 
+			out << "unknown architechture update version.cc ";
+
+#endif
+
+	out << std::endl;
+
+
+	/*
 	 * 2. Build time
 	 */
 
@@ -57,7 +90,7 @@ void getVersionInfo(std::ostream &out) {
 #endif
 
 	/*
-	 * 3. Various important build options in effect
+	 * 4. Various important build options in effect
 	 */
 
 	out << "Compile-time options: ";
@@ -179,27 +212,82 @@ void getVersionInfo(std::ostream &out) {
 	out << "Windows ";
 	{
 		// Get the version
-		OSVERSIONINFO info;
+		OSVERSIONINFOEXA info;
 		info.dwOSVersionInfoSize = sizeof(info);
-		GetVersionEx(&info);
+		GetVersionEx((LPOSVERSIONINFOA)&info);
 
 		// Platform is NT
 		if (info.dwPlatformId == VER_PLATFORM_WIN32_NT) {
-			if (info.dwMajorVersion < 4) out << "NT";
-			else if (info.dwMajorVersion == 4) out << "NT4";
-			else if (info.dwMajorVersion == 5 && info.dwMinorVersion == 0) out << 2000;
-			else if (info.dwMajorVersion == 5 && info.dwMinorVersion == 1) out << "XP";
-			else if (info.dwMajorVersion == 5 && info.dwMinorVersion == 2) out << 2003;
-			else if (info.dwMajorVersion == 6 && info.dwMinorVersion == 0) out << "Vista";
-			else if (info.dwMajorVersion == 6 && info.dwMinorVersion == 1) out << "7";
-			else if (info.dwMajorVersion == 6 && info.dwMinorVersion == 2) out << "8";
-			// Note: Without the proper manifest file, GetVersionEx will lie about these.
-			else if (info.dwMajorVersion == 6 && info.dwMinorVersion == 3) out << "8.1";
-			else if (info.dwMajorVersion == 10 && info.dwMinorVersion == 0) out << "10";
-			else out << "Unknown NT";
+			if (info.wProductType == VER_NT_WORKSTATION) {
+				if (info.dwMajorVersion < 4)
+					out << "NT";
+				else if (info.dwMajorVersion == 4)
+					out << "NT4";
+				else if (info.dwMajorVersion == 5 && info.dwMinorVersion == 0)
+					out << 2000;
+				else if (info.dwMajorVersion == 5 && info.dwMinorVersion == 1)
+					out << "XP";
+				// Only workstation release with version 5.2 was XP x64
+				else if (info.dwMajorVersion == 5 && info.dwMinorVersion == 2)
+					out << "XP";
+				else if (info.dwMajorVersion == 6 && info.dwMinorVersion == 0)
+					out << "Vista";
+				else if (info.dwMajorVersion == 6 && info.dwMinorVersion == 1)
+					out << "7";
+				else if (info.dwMajorVersion == 6 && info.dwMinorVersion == 2)
+					out << "8";
+				// Note: Without the proper manifest file, GetVersionEx will lie
+				// about these.
+				else if (info.dwMajorVersion == 6 && info.dwMinorVersion == 3)
+					out << "8.1";
+				else if (info.dwMajorVersion == 10) {
+					// cut off for Windows 10 and 11 is build 22000 (11 builds
+					// with a build number lower than 22000 are not public
+					// releases so I don't care)
+					if (LOWORD(info.dwBuildNumber & 0xFFFF) < 22000)
+						out << "10";
+					else
+						out << "11";
+				} else
+					out << "Unknown NT";
+			} else {    // Server
 
-			if (info.szCSDVersion[0]) out << " " << info.szCSDVersion;
+				if (info.dwMajorVersion < 4)
+					out << "NT Server";
+				else if (info.dwMajorVersion == 4)
+					out << "NT4 Server";
+				else if (info.dwMajorVersion == 5 && info.dwMinorVersion == 0)
+					out << "2000 Server";
+				// No Server release of 5.1
+				else if (info.dwMajorVersion == 5 && info.dwMinorVersion == 2)
+					out << " Windows Server 2003";
+				else if (info.dwMajorVersion == 6 && info.dwMinorVersion == 0)
+					out << "Windows Server 2008";
+				else if (info.dwMajorVersion == 6 && info.dwMinorVersion == 1)
+					out << "Windows Server 2008 R2";
+				else if (info.dwMajorVersion == 6 && info.dwMinorVersion == 2)
+					out << "Windows Server 2012";
+				// Note: Without the proper manifest file, GetVersionEx will lie
+				// about these.
+				else if (info.dwMajorVersion == 6 && info.dwMinorVersion == 3)
+					out << "Windows Server 2012 R2";
+				else if (info.dwMajorVersion == 10) {
+					if (LOWORD(info.dwBuildNumber & 0xFFFF) < 17000)
+						out << "Server 2016";
+					if (LOWORD(info.dwBuildNumber & 0xFFFF) < 19000)
+						out << "Server 2019";
+					else
+						out << "Server 2022";
+				} else
+					out << "Unknown NT Server";
+			}
+
+		} else {
+			out << "Unknown NT";
 		}
+		
+			if (info.szCSDVersion[0]) out << " " << info.szCSDVersion;
+		
 		else if (info.dwMajorVersion == 4 && info.dwMinorVersion == 0) {
 			out << 95;
 			if (info.szCSDVersion[1] != ' ') out << info.szCSDVersion;
@@ -210,7 +298,50 @@ void getVersionInfo(std::ostream &out) {
 		} else if (info.dwMajorVersion == 4 && info.dwMinorVersion == 90)
 			out << "Me";
 
-		out << " Version " << info.dwMajorVersion << "." << info.dwMinorVersion << " Build " << LOWORD(info.dwBuildNumber & 0xFFFF);
+		out << " Version " << info.dwMajorVersion << "." << info.dwMinorVersion
+			<< " Build " << LOWORD(info.dwBuildNumber & 0xFFFF) << " ";
+
+		// This function only exists in XP or newer but I see no reason to break compatibility with older windows version here so using it dynamically
+	void (WINAPI *fpGetNativeSystemInfo)(
+	   LPSYSTEM_INFO lpSystemInfo
+	) = nullptr;
+		HMODULE kernel32 = GetModuleHandleA("KERNEL32");
+	if (kernel32) {
+			*(FARPROC*)&fpGetNativeSystemInfo
+					= GetProcAddress(kernel32, "GetNativeSystemInfo");
+
+
+			// We default to GetSystemInfo (win2000 req) if we couldn't get GetNativeSystemInfo
+			if (!fpGetNativeSystemInfo)
+				*(FARPROC*)&fpGetNativeSystemInfo
+						= GetProcAddress(kernel32, "GetSystemInfo");
+	}
+		// If we have one of the get systeminfo functions print some details, mostly to know if its ARM64 or AMD64 OS running x86 code
+		if (fpGetNativeSystemInfo) {
+			SYSTEM_INFO sysinfo;
+			fpGetNativeSystemInfo(&sysinfo);
+			switch (sysinfo.wProcessorArchitecture) {
+			case PROCESSOR_ARCHITECTURE_INTEL:
+				out << "x86 ";
+				break;
+			case PROCESSOR_ARCHITECTURE_IA64:
+				out << "IA64 ";
+				break;
+			case PROCESSOR_ARCHITECTURE_ARM64:
+				out << "ARM64 ";
+				break;
+			case PROCESSOR_ARCHITECTURE_ARM:
+				out << "ARM ";
+				break;
+			case PROCESSOR_ARCHITECTURE_AMD64:
+				out << "x64 ";
+				break;
+			default:
+				out << "UnknownArch ";
+			}
+
+			out << sysinfo.dwNumberOfProcessors << " Processors";
+		}
 	}
 #elif (defined(FREEBSD))
 	out << "FreeBSD";
