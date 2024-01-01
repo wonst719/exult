3c047537b914676d551a6df09be08e2568b0b831
Marzo Sette Torres Junior <marzojr@gmail.com>
2024-01-01 21:30:45 +0100

    Fixing last -Wunused-variable

--- gumps/Gamemenu_gump.cc
+++ gumps/Gamemenu_gump.cc
@@ -54,7 +54,7 @@ static const char* displaytext  = "Game Display";
 static const char* enginetext   = "Game Engine";
 static const char* inputtext    = "Game Input";
 #ifndef __IPHONEOS__
-static const char* quitmenutext = "Quit to Menu";
+[[maybe_unused]] static const char* quitmenutext = "Quit to Menu";
 static const char* quittext     = "Quit";
 #endif
 
