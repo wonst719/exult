422d7e2833e731c46e8dc52916b3394f9a270e79
Alexis Ryan <the.wench@wenchy.net>
2024-01-15 12:36:14 +1100

    Tweaking Visual Studio Build Settings  and Nuget Packaages so x64 builds now properly work

--- msvcstuff/vs2019/msvc_include.h
+++ msvcstuff/vs2019/msvc_include.h
@@ -20,6 +20,10 @@
 #	pragma warning(disable : 4290)    // C++ exception specification ignored
 									   // except to indicate a function is not
 									   // __declspec(nothrow)
+
+#	pragma warning (disable : 4267 4244) // conversion from 'type' to 'type' possible loss of data
+#	pragma warning(disable : 4805) // unsafe mix of type 'int' and type 'bool' in operation
+
 #endif
 
 #include <windows.h>
@@ -28,3 +32,25 @@
 #define USE_WINDOWS_MIDI 1
 // #define USE_MT32EMU_MIDI 1
 #define HAVE_STDIO_H 1
+
+#if _MSC_VER <= 1938
+
+#include <algorithm>
+#include <cctype>
+
+#	pragma warning(disable : 4091) // 'typedef ' : ignored on left of 'tagGPFIDL_FLAGS' when no variable
+#	pragma warning(disable : 4309) // truncation of constant value 
+
+/*
+namespace std {
+	using ::toupper;
+	using ::tolower;
+	using ::isdigit;
+	using ::toupper;
+	using ::isalnum;
+	using ::isspace;
+	using ::isgraph;
+}    // namespace std
+*/
+
+#endif    //  _MSC_VER <= 1938
