40e0b3f9ea12863bcbb6920242d7594e69d545e2
Marzo Sette Torres Junior <marzojr@gmail.com>
2024-02-10 16:02:38 +0100

    Fixing build with ENABLE_MIDISFX

--- audio/Midi.cc
+++ audio/Midi.cc
@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../conf/Configuration.h"
 #include "Audio.h"
 #include "AudioMixer.h"
+#include "conv.h"
 #include "LowLevelMidiDriver.h"
 #include "MidiDriver.h"
 #include "OggAudioSample.h"
