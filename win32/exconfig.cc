wonst719/exult

commit 75f33f4f772fabc1b71fd7c64c8fabe342c79b9b

Tweaking exconfig behaviour and fixing a buffer overrun. It will now allow invalid paths to be set provided the existing path is not valid. It will always refuse to update exult.cfg with new paths that are empty or whitespace.

win32/exconfig.cc
@@ -311,15 +311,19 @@ __declspec(dllexport) void __stdcall GetExultGamePaths(
 	delete[] p;
 }
 
-bool IsNullOrWhitespace(const char*s)
+//
+// Check if a string is Null or Empty or Entirely whitespace
+//
+bool IsNullEmptyOrWhitespace(const char*s)
 {
-	// is null
-	if(s) while (*s++)
-	{
+	// if it is not null check for non white space charactere
+	if(s)
+		for (; *s; ++s) {
 		// if any charaxter is not space and not blank return false;
 		if (!std::isspace(*s) && !std::isblank(*s))
 			return false;
 	}
+	// string is null or has no non whitespace characters
 	return true;
 }
 
@@ -351,11 +355,15 @@ __declspec(dllexport) void __stdcall SetExultGamePaths(
 		} else {
 			config.read_config_file("exult.cfg");
 		}
-
+		char existing_bgpath[MAX_PATH];
+		char existing_sipath[MAX_PATH];
+		GetExultGamePaths(ExultDir, existing_bgpath, existing_sipath, MAX_PATH);
+		
 		// Set BG Path
 		MessageBoxDebug(nullptr, p, "WriteConfig: BG", MB_OK);
-		// Only write new path if it is actually valid
-		if (!IsNullOrWhitespace(BGPath) && VerifyBGDirectory(BGPath)) {
+		
+		// Only write new path if it is actually valid or existing value is invalid
+		if (!IsNullEmptyOrWhitespace(BGPath) && (VerifyBGDirectory(BGPath) || !VerifyBGDirectory(existing_bgpath))) {
 			config.set("config/disk/game/blackgate/path", BGPath, true);
 			// Only write default if there isn't already a value set.
 		} else if (!config.key_exists("config/disk/game/blackgate/path")) {
@@ -363,8 +371,8 @@ __declspec(dllexport) void __stdcall SetExultGamePaths(
 		}
 		// Set SI Path
 		MessageBoxDebug(nullptr, p, "WriteConfig: SI", MB_OK);
-		// Only write new path if it is actually valid
-		if (!IsNullOrWhitespace(SIPath) && VerifySIDirectory(SIPath)) {
+		// Only write new path if it is actually valid or existing value is invalid
+		if (!IsNullEmptyOrWhitespace(SIPath) && (VerifySIDirectory(SIPath) || !VerifyBGDirectory(existing_sipath))) {
 			config.set("config/disk/game/serpentisle/path", SIPath, true);
 			// Only write default if there isn't already a value set.
 		} else if (!config.key_exists("config/disk/game/serpentisle/path")) {
@@ -408,6 +416,8 @@ __declspec(dllexport) void __stdcall SetExultGamePaths(
 // Verify the BG Directory contains the right stuff
 //
 __declspec(dllexport) int __stdcall VerifyBGDirectory(char* path) {
+	if(IsNullEmptyOrWhitespace(path))
+		return false;
 	const std::string s(path);
 	add_system_path("<STATIC>", s + "/static");
 
@@ -445,6 +455,8 @@ __declspec(dllexport) int __stdcall VerifyBGDirectory(char* path) {
 // Verify the SI Directorys contains the right stuff
 //
 __declspec(dllexport) int __stdcall VerifySIDirectory(char* path) {
+	if(IsNullEmptyOrWhitespace(path))
+		return false;
 	const std::string s(path);
 	add_system_path("<STATIC>", s + "/static");
 

commit 5246ecbf63cb8957253bd3ff15c8e19d64387b6b

Fixing installer trashing SI path in exult.cfg for #432 corrects bug introduced in  75f33f4

win32/exconfig.cc
@@ -259,7 +259,8 @@ __declspec(dllexport) void __stdcall GetExultGamePaths(
 	MessageBoxDebug(nullptr, ExultDir, "ExultDir", MB_OK);
 	MessageBoxDebug(nullptr, BGPath, "BGPath", MB_OK);
 	MessageBoxDebug(nullptr, SIPath, "SIPath", MB_OK);
-
+	std::memset(BGPath, 0, MaxPath);
+	std::memset(SIPath, 0, MaxPath);
 	const int p_size = strlen(ExultDir) + strlen("/exult.cfg") + MAX_STRLEN;
 	char*     p      = new char[p_size];
 
@@ -314,14 +315,15 @@ __declspec(dllexport) void __stdcall GetExultGamePaths(
 //
 // Check if a string is Null or Empty or Entirely whitespace
 //
-bool IsNullEmptyOrWhitespace(const char*s)
-{
+bool IsNullEmptyOrWhitespace(const char* s) {
 	// if it is not null check for non white space charactere
-	if(s)
+	if (s) {
 		for (; *s; ++s) {
-		// if any charaxter is not space and not blank return false;
-		if (!std::isspace(*s) && !std::isblank(*s))
-			return false;
+			// if any charaxter is not space and not blank return false;
+			if (!std::isspace(*s) && !std::isblank(*s)) {
+				return false;
+			}
+		}
 	}
 	// string is null or has no non whitespace characters
 	return true;
@@ -358,12 +360,15 @@ __declspec(dllexport) void __stdcall SetExultGamePaths(
 		char existing_bgpath[MAX_PATH];
 		char existing_sipath[MAX_PATH];
 		GetExultGamePaths(ExultDir, existing_bgpath, existing_sipath, MAX_PATH);
-		
+
 		// Set BG Path
 		MessageBoxDebug(nullptr, p, "WriteConfig: BG", MB_OK);
-		
-		// Only write new path if it is actually valid or existing value is invalid
-		if (!IsNullEmptyOrWhitespace(BGPath) && (VerifyBGDirectory(BGPath) || !VerifyBGDirectory(existing_bgpath))) {
+
+		// Only write new path if it is actually valid or existing value is
+		// invalid
+		if (!IsNullEmptyOrWhitespace(BGPath)
+			&& (VerifyBGDirectory(BGPath)
+				|| !VerifyBGDirectory(existing_bgpath))) {
 			config.set("config/disk/game/blackgate/path", BGPath, true);
 			// Only write default if there isn't already a value set.
 		} else if (!config.key_exists("config/disk/game/blackgate/path")) {
@@ -371,8 +376,11 @@ __declspec(dllexport) void __stdcall SetExultGamePaths(
 		}
 		// Set SI Path
 		MessageBoxDebug(nullptr, p, "WriteConfig: SI", MB_OK);
-		// Only write new path if it is actually valid or existing value is invalid
-		if (!IsNullEmptyOrWhitespace(SIPath) && (VerifySIDirectory(SIPath) || !VerifyBGDirectory(existing_sipath))) {
+		// Only write new path if it is actually valid or existing value is
+		// invalid
+		if (!IsNullEmptyOrWhitespace(SIPath)
+			&& (VerifySIDirectory(SIPath)
+				|| !VerifySIDirectory(existing_sipath))) {
 			config.set("config/disk/game/serpentisle/path", SIPath, true);
 			// Only write default if there isn't already a value set.
 		} else if (!config.key_exists("config/disk/game/serpentisle/path")) {
@@ -416,8 +424,9 @@ __declspec(dllexport) void __stdcall SetExultGamePaths(
 // Verify the BG Directory contains the right stuff
 //
 __declspec(dllexport) int __stdcall VerifyBGDirectory(char* path) {
-	if(IsNullEmptyOrWhitespace(path))
+	if (IsNullEmptyOrWhitespace(path)) {
 		return false;
+	}
 	const std::string s(path);
 	add_system_path("<STATIC>", s + "/static");
 
@@ -455,8 +464,9 @@ __declspec(dllexport) int __stdcall VerifyBGDirectory(char* path) {
 // Verify the SI Directorys contains the right stuff
 //
 __declspec(dllexport) int __stdcall VerifySIDirectory(char* path) {
-	if(IsNullEmptyOrWhitespace(path))
+	if (IsNullEmptyOrWhitespace(path)) {
 		return false;
+	}
 	const std::string s(path);
 	add_system_path("<STATIC>", s + "/static");
 

win32/exult_installer.iss
@@ -376,8 +376,8 @@ end;
 //
 function ShouldSkipPage(PageID: Integer): Boolean;
 var
-  sBGPath: String;
-  sSIPath: String;
+  sBGPath: AnsiString;
+  sSIPath: AnsiString;
 begin
   if PageID = DataDirPage.ID then begin
     Result := (WizardIsComponentSelected('Paths') = False);