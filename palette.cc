1ebd345c30d70b78bda8bea18c2b535403a0bc16
Alexis Ryan <the.wench@wenchy.net>
2024-07-05 20:53:48 +1000

    Slightly improved handling of palette ramps in the cycling region ofthe palette

--- palette.cc
+++ palette.cc
@@ -32,6 +32,7 @@
 #include "ignore_unused_variable_warning.h"
 #include "utils.h"
 
+#include <algorithm>
 #include <cstring>
 
 #ifdef __GNUC__
@@ -525,12 +526,19 @@ const Palette::Ramp* Palette::get_ramps(unsigned int& num_ramps) {
 		unsigned int r    = 0;
 		int          last = pal1[3] + pal1[4] + pal1[5];
 		ramps[0].start    = 1;
+		// Where the palette sycling ramps start
+		uint8 palette_cycling[] = {
+				0xe0, 0xe8,0xf0, 0xf4,0xf8,0xfc, 0xff,
+		};
+		
 		for (int c = 2; c < 256; c++) {
 			int brightness = pal1[c * 3] + pal1[c * 3 + 1] + pal1[c * 3 + 2];
 
 			// Big change in brightness means start of a ramp (note 6 bit colour
 			// so 48 is equiv to 192 in 8 bit)
-			if (std::abs(brightness - last) > 48) {
+			// Special handling for palette cycling ranges so they behave better
+			if ((c < palette_cycling[0] && std::abs(brightness - last) > 48)
+				|| c == *std::find(palette_cycling,palette_cycling + std::size(palette_cycling)- 1, c)) {
 				// set the end point of the previous ramp
 				ramps[r].end = c - 1;
 				r++;
