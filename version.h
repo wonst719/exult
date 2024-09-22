5da9ecfb92658736da1f6e4a4441fbd7238b0afb
Alexis Ryan <the.wench@wenchy.net>
2024-09-22 21:17:32 +1000

    Adding some x86 CPUID output to version.cc for common compilers. Exult will now report basic x86 cpu info to stdout, and will report warnings if Exult was built with compile flaags requiring CPU features not supported by the current CPU

--- version.h
+++ version.h
@@ -26,4 +26,7 @@ void             getVersionInfo(std::ostream& out);
 std::string_view VersionGetGitRevision(bool shortrev);
 std::string      VersionGetGitInfo(bool limitedwidth = false);
 
+bool OutputCPUID(std::ostream* out);
+bool CPUIDHasUnsuportedFeatures();
+
 #endif
