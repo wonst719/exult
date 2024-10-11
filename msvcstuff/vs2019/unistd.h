792ec929928ce251af51bf3c1c7465675d9ce468
Alexis Ryan <the.wench@wenchy.net>
2024-10-12 00:12:19 +1100

    Updating msvc projects. Removing hacky sdl headers, adding dirent vcpkg dependency removing need for my minimal header implementation. Added fluidsynth to msvc build. Added an msvc exult_studio project. As of this commit Exult studio will build but is not usable. Addded building exult_studio to ci_msvc

--- msvcstuff/vs2019/unistd.h
+++ msvcstuff/vs2019/unistd.h
@@ -3,4 +3,11 @@
 #ifndef UNISTD_H
 #define UNISTD_H
 
+// if we have the getopt from vcpkg include it for exult studio
+#	if __has_include(<getopt.h>)
+#include <getopt.h>
+#define HAVE_GETOPT_LONG 1
+#endif
+
+
 #endif UNISTD_H
