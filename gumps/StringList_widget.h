66f2f51199becce1193303d938689b7dbed5f236
Alexis Ryan <the.wench@wenchy.net>
2025-02-23 20:13:33 +1100

    Adding missing GPL header

--- gumps/StringList_widget.h
+++ gumps/StringList_widget.h
@@ -1,4 +1,24 @@
-#pragma once
+/*
+Copyright (C) 2025 The Exult Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#ifndef STRINGLIST_WIDGET_H
+#define STRINGLIST_WIDGET_H
+
 #include "Gump_widget.h"
 #include "Modal_gump.h"
 #include <string_view>
@@ -74,3 +94,4 @@ public:
 
 private:
 };
+#endif
