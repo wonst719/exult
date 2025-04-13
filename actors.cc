f472736b32fc77da566f7f74bf6cafa713029ada
DominusExult <domiman+github@gmail.com>
2025-04-13 16:48:32 +0200

    Fixes #663
    Already slain NPCs will not teleport (especially after the banes are released, this would leave duplicate bodies of already dead NPCs)

--- actors.cc
+++ actors.cc
@@ -5543,6 +5543,10 @@ void Npc_actor::switched_chunks(
  */
 
 void Npc_actor::move(int newtx, int newty, int newlift, int newmap) {
+	// prevent teleporting if dead
+	if (is_dead()) {
+		return;
+	}
 	// Store old chunk list.
 	Map_chunk* olist = get_chunk();
 	// Move it.
