wonst719/exult

commit ddb6fc2ca715d2febab1a7aac052024f291892ac

A new default folder "demo" for a demo game. Devel games will now always load even if there is no initgame.dat (yet). There are more cases that Exult will throw an exception if a file is missing and a devel game is not set to editing but this one is most important. If a static identity is not one of the U7 games it is now assumed that a game is of the type EXULT_DEVEL_GAME otherwise Exult would become very unstable. In #557 is a zipped up demo game.

gamemgr/modmgr.cc
@@ -1,7 +1,7 @@
 /*
  *  modmgr.cc - Mod manager for Exult.
  *
- *  Copyright (C) 2006-2022  The Exult Team
+ *  Copyright (C) 2006-2025  The Exult Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -368,16 +368,30 @@ ModManager::ModManager(
 	found = U7exists(initgam_path);
 
 	string static_identity;
+	char   fname[50];
+	string id = get_game_identity(fname, cfgname);
 	if (found) {
 		static_identity = get_game_identity(initgam_path.c_str(), cfgname);
+		if (static_identity != "ULTIMA7" && static_identity != "FORGE"
+			&& static_identity != "SERPENT ISLE"
+			&& static_identity != "SILVER SEED") {
+			static_identity = "DEVEL GAME";
+		}
 		if (!silent) {
 			cout << "found game with identity '" << static_identity << "'"
 				 << endl;
 		}
-	} else {    // New game still under development.
+	} else if (id == CFG_DEMO_NAME && U7exists(game_path)) {
 		static_identity = "DEVEL GAME";
 		if (!silent) {
-			cout << "but it wasn't there." << endl;
+			cout << "found Demo game with identity '" << static_identity << "'"
+				 << endl;
+		}
+	} else if (U7exists(game_path)) {    // New game still under development.
+		static_identity = "DEVEL GAME";
+		if (!silent) {
+			cout << "found game with identity '" << static_identity << "'"
+				 << endl;
 		}
 	}
 
@@ -490,12 +504,19 @@ ModManager::ModManager(
 			new_title = CFG_SS_TITLE;
 		}
 	} else {
-		type        = EXULT_DEVEL_GAME;
-		language    = ENGLISH;
-		expansion   = false;
-		sibeta      = false;
-		path_prefix = "DEVEL" + to_uppercase(name);
-		new_title   = menu;    // To be safe.
+		type      = EXULT_DEVEL_GAME;
+		language  = ENGLISH;
+		expansion = false;
+		sibeta    = false;
+		if (id == CFG_DEMO_NAME) {
+			path_prefix = to_uppercase(CFG_DEMO_NAME);
+			if (needtitle) {
+				new_title = CFG_DEMO_TITLE;
+			}
+		} else {
+			path_prefix = "DEVEL" + to_uppercase(name);
+			new_title   = menu;    // To be safe.
+		}
 	}
 
 	// If the "default" path selected above is already taken, then use a unique
@@ -616,7 +637,7 @@ void ModManager::add_mod(const string& mod, const string& modconfig) {
 // Checks the game 'gam' for the presence of the mod given in 'arg_modname'
 // and checks the mod's compatibility. If the mod exists and is compatible with
 // Exult, returns a reference to the mod; otherwise, returns the mod's parent
-// game. Outputs error messages is the mod is not found or is not compatible.
+// game. Outputs error messages if the mod is not found or is not compatible.
 BaseGameInfo* ModManager::get_mod(const string& name, bool checkversion) {
 	ModInfo* newgame = nullptr;
 	if (has_mods()) {
@@ -683,7 +704,7 @@ void ModManager::get_game_paths(const string& game_path) {
 // GameManager: class that manages the installed games
 GameManager::GameManager(bool silent) {
 	games.clear();
-	bg = fov = si = ss = sib = nullptr;
+	bg = fov = si = ss = sib = dev = nullptr;
 
 	// Search for games defined in exult.cfg:
 	const string              config_path("config/disk/game");
@@ -692,18 +713,19 @@ GameManager::GameManager(bool silent) {
 	std::vector<string>       checkgames;
 	checkgames.reserve(
 			gamestrs.size()
-			+ 5);    // +5 in case the four below are not in the cfg.
+			+ 6);    // +6 in case the six below are not in the cfg.
 	// The original games plus expansions.
 	checkgames.emplace_back(CFG_BG_NAME);
 	checkgames.emplace_back(CFG_FOV_NAME);
 	checkgames.emplace_back(CFG_SI_NAME);
 	checkgames.emplace_back(CFG_SS_NAME);
 	checkgames.emplace_back(CFG_SIB_NAME);
+	checkgames.emplace_back(CFG_DEMO_NAME);
 
 	for (const auto& gamestr : gamestrs) {
 		if (gamestr != CFG_BG_NAME && gamestr != CFG_FOV_NAME
 			&& gamestr != CFG_SI_NAME && gamestr != CFG_SS_NAME
-			&& gamestr != CFG_SIB_NAME) {
+			&& gamestr != CFG_SIB_NAME && gamestr != CFG_DEMO_NAME) {
 			checkgames.push_back(gamestr);
 		}
 	}
@@ -714,6 +736,7 @@ GameManager::GameManager(bool silent) {
 	int siind  = -1;
 	int ssind  = -1;
 	int sibind = -1;
+	int devind = -1;
 
 	for (const auto& gameentry : checkgames) {
 		// Load the paths for all games found:
@@ -729,7 +752,8 @@ GameManager::GameManager(bool silent) {
 		// This checks static identity and sets game type.
 		const ModManager game
 				= ModManager(gameentry, game_title, need_title, silent);
-		if (!game.being_edited() && !game.is_there()) {
+		if (!game.being_edited() && !game.is_there()
+			&& game.get_game_type() != EXULT_DEVEL_GAME) {
 			continue;
 		}
 		if (game.get_game_type() == BLACK_GATE) {
@@ -752,6 +776,10 @@ GameManager::GameManager(bool silent) {
 			} else if (siind == -1) {
 				siind = games.size();
 			}
+		} else if (game.get_game_type() == EXULT_DEVEL_GAME) {
+			if (devind == -1) {
+				devind = games.size();
+			}
 		}
 
 		games.push_back(game);
@@ -771,6 +799,10 @@ GameManager::GameManager(bool silent) {
 	}
 	if (sibind >= 0) {
 		sib = &(games[sibind]);
+	}
+
+	if (devind >= 0) {
+		dev = &(games[devind]);
 	}
 
 	// Sane defaults.

fnames.h
@@ -2,7 +2,7 @@
  *  fnames.h - Names of data files for Exult.
  *
  *  Copyright (C) 1999  Jeffrey S. Freedman
- *  Copyright (C) 2000-2022  The Exult Team
+ *  Copyright (C) 2000-2025  The Exult Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -189,6 +189,7 @@
 #define CFG_SI_ES_NAME "islaserpiente"
 #define CFG_SS_NAME    "silverseed"
 #define CFG_SIB_NAME   "serpentbeta"
+#define CFG_DEMO_NAME  "demo"
 
 // U7 game titles in "exult.cfg":
 #define CFG_BG_TITLE    "ULTIMA VII\nTHE BLACK GATE"
@@ -200,6 +201,7 @@
 #define CFG_SI_ES_TITLE "ULTIMA VII PART 2\nLA ISLA SERPIENTE"
 #define CFG_SS_TITLE    "ULTIMA VII PART 2\nTHE SILVER SEED"
 #define CFG_SIB_TITLE   "ULTIMA VII PART 2\nSERPENT ISLE BETA"
+#define CFG_DEMO_TITLE  "EXULT\nDEMO GAME"
 
 // Exult SFX Packages:
 #define SFX_ROLAND_BG  "sqsfxbg.flx"

gamedat.cc
@@ -1,7 +1,7 @@
 /*
  *  gamedat.cc - Create gamedat files from a savegame.
  *
- *  Copyright (C) 2000-2022  The Exult Team
+ *  Copyright (C) 2000-2025  The Exult Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -201,7 +201,10 @@ void Game_window::restore_gamedat(const char* fname    // Name of savegame file.
 	// trailing slash
 	IFileDataSource in(fname);
 	if (!in.good()) {
-		if (!Game::is_editing()) {    // Ok if map-editing.
+		if (!Game::is_editing()
+			&& Game::get_game_type()
+					   != EXULT_DEVEL_GAME) {    // Ok if map-editing or devel
+												 // game.
 			throw file_read_exception(fname);
 		}
 		std::cerr << "Warning (map-editing): Couldn't open '" << fname << "'"

gamemgr/modmgr.h
@@ -1,7 +1,7 @@
 /*
  *  modmgr.h - Mod manager for Exult.
  *
- *  Copyright (C) 2006-2022  The Exult Team
+ *  Copyright (C) 2006-2025  The Exult Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -246,6 +246,7 @@ protected:
 	ModManager*             si;
 	ModManager*             ss;
 	ModManager*             sib;
+	ModManager*             dev;
 	std::vector<ModManager> games;
 	void                    print_found(
 							   ModManager* game, const char* flex, const char* title,
@@ -286,6 +287,10 @@ public:
 		return sib != nullptr;
 	}
 
+	bool is_devel_installed() const {
+		return dev != nullptr;
+	}
+
 	ModManager* find_game(const std::string& name);
 
 	ModManager* get_bg() {
@@ -306,6 +311,10 @@ public:
 
 	ModManager* get_sib() {
 		return sib;
+	}
+
+	ModManager* get_devel() {
+		return dev;
 	}
 
 	int  find_game_index(const std::string& name);

mapedit/studio.cc
@@ -1512,6 +1512,10 @@ void ExultStudio::set_game_path(const string& gamename, const string& modname) {
 		if (!(basegame = gamemanager->get_ss())) {
 			cerr << "Silver Seed not found." << endl;
 		}
+	} else if (gamename == CFG_DEMO_NAME) {
+		if (!(basegame = gamemanager->get_devel())) {
+			cerr << "Demo Game not found." << endl;
+		}
 	} else {
 		if ((curr_game = gamemanager->find_game_index(gamename)) < 0) {
 			cerr << "Game '" << gamename << "' not found." << endl;

commit 11e1461bf9f686b1a593960911fe61ba82dd026d

fix bug introduced with ddb6fc2ca which listed not installed default games in the menu

gamemgr/modmgr.cc
@@ -502,7 +502,7 @@ ModManager::ModManager(
 		if (needtitle) {
 			new_title = CFG_SS_TITLE;
 		}
-	} else {
+	} else if (static_identity == "DEVEL GAME") {
 		type      = EXULT_DEVEL_GAME;
 		language  = ENGLISH;
 		expansion = false;