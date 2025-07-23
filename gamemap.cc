wonst719/exult

commit d927d36bd9dd48243f281b6655304de5671fce5f

Properly fix #449.
The previous fix seemed as if it appened duplicated chunks to the chunk list. But upon saving and restarting it would in fact be placed next to the duplicated chunk.

gamemap.cc
@@ -32,6 +32,7 @@
 #include "actors.h" /* For Dead_body, which should be moved. */
 #include "animate.h"
 #include "barge.h"
+#include "cheat.h"
 #include "chunks.h"
 #include "databuf.h"
 #include "effects.h"
@@ -78,6 +79,7 @@ std::unique_ptr<std::istream> Game_map::chunks;
 bool                          Game_map::v2_chunks               = false;
 bool                          Game_map::read_all_terrain        = false;
 bool                          Game_map::chunk_terrains_modified = false;
+bool                          Game_map::map_modified            = false;
 
 const int   V2_CHUNK_HDR_SIZE = 4 + 4 + 2;    // 0xffff, "exlt", vers.
 static char v2hdr[]
@@ -132,7 +134,7 @@ Chunk_terrain* Game_map::read_terrain(
  */
 
 Game_map::Game_map(int n)
-		: num(n), didinit(false), map_modified(false), caching_out(0),
+		: num(n), didinit(false), caching_out(0),
 		  map_patches(std::make_unique<Map_patch_collection>()) {}
 
 /*
@@ -1381,6 +1383,40 @@ bool Game_map::insert_terrain(
 ) {
 	const int ntiles = c_tiles_per_chunk * c_tiles_per_chunk;
 	const int nbytes = v2_chunks ? 3 : 2;
+	if (dup && tnum == -2) {
+		const int selected = cheat.get_edit_chunknum();
+		if (selected < 0
+			|| selected >= static_cast<int>(chunk_terrains->size())) {
+			return false;    // Invalid #.
+		}
+		get_all_terrain();                // Need all of 'u7chunks' read in.
+		unsigned char buf[ntiles * 3];    // Set up buffer with shape #'s.
+		// Want to duplicate the selected terrain.
+		Chunk_terrain* ter  = (*chunk_terrains)[selected];
+		unsigned char* data = &buf[0];
+		for (int ty = 0; ty < c_tiles_per_chunk; ty++) {
+			for (int tx = 0; tx < c_tiles_per_chunk; tx++) {
+				const ShapeID id    = ter->get_flat(tx, ty);
+				const int     shnum = id.get_shapenum();
+				const int     frnum = id.get_framenum();
+				if (v2_chunks) {
+					Write1(data, shnum & 0xff);
+					Write1(data, (shnum >> 8) & 0xff);
+					Write1(data, frnum);
+				} else {
+					Write1(data, id.get_shapenum() & 0xff);
+					Write1(data, ((id.get_shapenum() >> 8) & 3)
+										 | (id.get_framenum() << 2));
+				}
+			}
+		}
+		auto* new_terrain = new Chunk_terrain(&buf[0], v2_chunks);
+		new_terrain->set_modified();
+		chunk_terrains->push_back(new_terrain);
+		chunk_terrains_modified = true;
+		map_modified            = true;
+		return true;
+	}
 	if (tnum < -1 || tnum >= static_cast<int>(chunk_terrains->size())) {
 		return false;    // Invalid #.
 	}

gamemap.h
@@ -64,7 +64,7 @@ class Game_map {
 	static bool read_all_terrain;    // True if we've read them all.
 	static bool chunk_terrains_modified;
 	bool        didinit;
-	bool        map_modified;    // True if any map changes from
+	static bool map_modified;    // True if any map changes from
 	//   map-editing.
 	// Chunk_terrain index for each chunk:
 	short terrain_map[c_num_chunks][c_num_chunks];
@@ -117,7 +117,7 @@ public:
 		map_modified = true;
 	}
 
-	bool was_map_modified() const {
+	static bool was_map_modified() {
 		return map_modified;
 	}
 

mapedit/chunklst.cc
@@ -999,8 +999,9 @@ void Chunk_chooser::insert(bool dup) {
 		return;    // Shouldn't happen.
 	}
 	unsigned char  data[Exult_server::maxlength];
-	unsigned char* ptr  = &data[0];
-	const int      tnum = selected >= 0 ? info[selected].num : -1;
+	unsigned char* ptr = &data[0];
+	// Send special value -2 to indicate "append duplicate to end"
+	const int tnum = -2;
 	little_endian::Write2(ptr, tnum);
 	Write1(ptr, dup ? 1 : 0);
 	ExultStudio* studio = ExultStudio::get_instance();
@@ -1033,8 +1034,9 @@ void Chunk_chooser::insert_response(const unsigned char* data, int datalen) {
 	} else {
 		// Insert in our list.
 		auto* data = new unsigned char[chunksz];
-		if (dup && tnum >= 0 && tnum < num_chunks && chunklist[tnum]) {
-			memcpy(data, chunklist[tnum], chunksz);
+		if (dup && selected >= 0 && selected < num_chunks
+			&& chunklist[info[selected].num]) {
+			memcpy(data, chunklist[info[selected].num], chunksz);
 		} else {
 			memset(data, 0, chunksz);
 		}
@@ -1044,7 +1046,9 @@ void Chunk_chooser::insert_response(const unsigned char* data, int datalen) {
 		// if (tnum >= 0 && tnum < num_chunks - 1) {
 		//	chunklist.insert(chunklist.begin() + tnum + 1, data);
 		// } else    // If -1, append to end.
-		chunklist.push_back(data);
+		if (tnum >= -2 && tnum < num_chunks - 1) {
+			chunklist.push_back(data);
+		}
 
 		update_num_chunks(num_chunks + 1);
 		render();

commit ef904fcbbade1ebcd6d7bdae8f31c6dfa3b87a86

Creating new empty chunk was broken in d927d36bd.

gamemap.cc
@@ -1416,6 +1416,18 @@ bool Game_map::insert_terrain(
 		chunk_terrains_modified = true;
 		map_modified            = true;
 		return true;
+	} else if (!dup && tnum == -2) {
+		// Special case for creating an empty chunk and appending it
+		get_all_terrain();
+		unsigned char buf[ntiles * 3];
+		memset(buf, 0, sizeof(buf));    // Create an empty chunk
+
+		auto* new_terrain = new Chunk_terrain(&buf[0], v2_chunks);
+		new_terrain->set_modified();
+		chunk_terrains->push_back(new_terrain);
+		chunk_terrains_modified = true;
+		map_modified            = true;
+		return true;
 	}
 	if (tnum < -1 || tnum >= static_cast<int>(chunk_terrains->size())) {
 		return false;    // Invalid #.