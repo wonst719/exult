f4c8fb6375d4e4bd6ff868fff9e502cba4f69128
Alexis Ryan <the.wench@wenchy.net>
2025-09-25 23:28:27 +1000

    adding Font::get_text_box_dims method

--- shapes/font.cc
+++ shapes/font.cc
@@ -656,6 +656,32 @@ int Font::get_text_width(
 	return width;
 }
 
+void Font::get_text_box_dims(
+		const char* text, int& width, int& height, int vert_lead) {
+	width         = 0;
+	height        = 0;
+	int cur_width = 0;
+
+	int num_lines = 1;
+	if (font_shapes) {
+		short chr;
+		while ((chr = *text++) != 0) {
+			if (chr == '\n') {
+				num_lines++;
+				width     = std::max(width, cur_width);
+				cur_width = 0;
+			}
+			Shape_frame* shape
+					= font_shapes->get_frame(static_cast<unsigned char>(chr));
+			if (shape && shape->is_rle()) {
+				cur_width += shape->get_width() + hor_lead;
+			}
+		}
+		width  = std::max(width, cur_width);
+		height = num_lines * (get_text_height() + vert_lead + ver_lead);
+	}
+}
+
 /*
  *  Get font line-height.
  */
