1acc876e6df037df2815ba97934fcb829c24a628
Alexis Ryan <the.wench@wenchy.net>
2025-09-26 22:21:27 +1000

    update Gump::set_pos() and Gump_widget::set_pos() so they update the gwin dirty region to ensure the needed regions of the screen are redrawn

--- gumps/Gump_widget.h
+++ gumps/Gump_widget.h
@@ -99,10 +99,7 @@ public:
 		return y;
 	}
 
-	void set_pos(int newx, int newy) override {
-		x = newx;
-		y = newy;
-	}
+	void set_pos(int newx, int newy) override;
 
 	void screen_to_local(int& sx, int& sy) const override {
 		sx -= x;
