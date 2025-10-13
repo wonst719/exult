e1b5e5f79870f0d53001a68ed9f33ad29d3e6256
DominusExult <domiman+github@gmail.com>
2025-10-13 09:10:51 +0200

    moved comment

--- gamerend.cc
+++ gamerend.cc
@@ -597,7 +597,8 @@ int Game_render::paint_chunk_objects(
 		for (const auto& obj : lights) {
 			const Shape_info& info = obj->get_info();
 			if (info.get_object_light(obj->get_framenum())
-				> 0) {    // Count light sources.
+				> 0) {
+				// Count light sources.
 				light_sources += get_light_strength(obj, main_actor);
 			}
 		}
