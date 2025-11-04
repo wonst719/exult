d19e77ffa4a9fb9f66f67b8105ff69b0625e02e8
Marzo Sette Torres Junior <marzojr@gmail.com>
2025-11-04 20:41:22 +0100

    One more case for v2 chunks

--- objs/chunkter.cc
+++ objs/chunkter.cc
@@ -357,7 +357,8 @@ bool Chunk_terrain::need_extended_shapes() const {
 		for (int tx = 0; tx < c_tiles_per_chunk; tx++) {
 			const ShapeID id       = get_flat(tx, ty);
 			const int     shapenum = id.get_shapenum();
-			if (shapenum > 0x3ff) {
+			const int     framenum = id.get_framenum();
+			if (shapenum > 0x3ff || framenum > 0x3f) {
 				return true;
 			}
 		}
