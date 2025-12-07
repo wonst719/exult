1a9337b4b7483fc8be1d80e40ed388b07ea7b152
DominusExult <domiman+github@gmail.com>
2025-12-08 00:26:56 +0100

    Lots of layout work on Exult Studio. Not perfect but the ui is looking better and consistent now. The Exult Shape Info dialog isn't allowing resizing anymore with the benefit that it will still adapt its size for the different VGA files. It's shape drawing will also reset to 150x150 to prevent large shapes setting the drawing too large when viewing smaller shapes later on.

--- mapedit/shapeedit.cc
+++ mapedit/shapeedit.cc
@@ -4845,6 +4845,13 @@ void ExultStudio::open_shape_window(
 		gtk_widget_set_visible(gump_notebook, false);
 	}
 
+	// Reset the main shape drawing to a smaller size each time shape_window is
+	// opened.
+	GtkWidget* draw_area = get_widget("shinfo_draw");
+	if (draw_area) {
+		gtk_widget_set_size_request(draw_area, 150, 150);
+	}
+
 	gtk_widget_set_visible(shapewin, true);
 }
 
